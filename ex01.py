f77a4e5ca3c36e522548250eed32283b3cc9d0b0
park1997 <iambyunghyun97@gmail.com>
2020-11-15 16:56:24 +0900

    Update ex01.py

--- ex01.py
+++
@@ -1,8 +0,0 @@
-import sys
-a,b=map(int,sys.stdin.readline().split())
-nums=list(map(int,sys.stdin.readline().split()))
-if nums.count(1)<b:
-    print(-1)
-else:
-    lion=[i for i,j in enumerate(nums) if j==1]
-    print(min((lion[b-1+k]-lion[k]+1) for k in range(len(lion)-b+1)))
