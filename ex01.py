ceac1ec4fa2ec81412cf80fdb67328c41c0d3859
park1997 <iambyunghyun97@gmail.com>
2020-12-23 17:13:59 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,8 +1,6 @@
 import sys
-a=sys.stdin.readline().rstrip()
-num=[int(i) for i in a]
-if ("0" not in a) or (sum(num)%3!=0):
-    print(-1)
-else:
-    num=sorted(num,reverse=True)
-    print(*num,sep="")
+a,b=map(str,sys.stdin.readline().split())
+c=""
+cnt=0
+if len(a)<len(b):
+    
