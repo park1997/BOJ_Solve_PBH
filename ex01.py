d0c7cfd2cd4070b6e8a5ab24946dd3f39271de40
park1997 <iambyunghyun97@gmail.com>
2021-01-23 02:04:39 +0900

    Update ex01.py

--- ex01.py
+++
@@ -1,7 +0,0 @@
-num=[list(map(int,input().split())) for i in range(int(input()))]
-num.append([num[0][0],num[0][1]])
-x,y=0,0
-for i in range(len(num)-1):
-    x+=num[i][0]*num[i+1][1]
-    y+=num[i][1]*num[i+1][0]
-print(round(abs(x-y)/2,2))
