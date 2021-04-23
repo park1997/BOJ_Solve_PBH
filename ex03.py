c1e2481baed2fa78e0d2220a0e80ab7c7b01ac95
park1997 <iambyunghyun97@gmail.com>
2021-04-23 20:54:34 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,19 +1,12 @@
-from collections import deque
-N, M = map(int, input().split())
-miro = []
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-for i in range(N):
-    miro.append(list(input()))
-q= deque()
-q.append([0,0])
-miro[0][0] = 1
-while q:
-    a, b = q.popleft()
-    for i in range(4):
-        x = a + dx[i]
-        y = b + dy[i]
-        if 0 <= x < N and 0 <= y < M and miro[x][y] == "1":
-            q.append([x, y])
-            miro[x][y] = miro[a][b] + 1
-print(miro[N-1][M-1])
+import sys
+import bisect
+n=int(sys.stdin.readline())
+arr = list(map(int,sys.stdin.readline().split()))
+result=[arr[0]]
+for i in range(1,n):
+    if result[-1]<arr[i]:
+        result.append(arr[i])
+    else:
+        idx = bisect.bisect_left(result,arr[i])
+        result[idx] = arr[i]
+print(len(result))
