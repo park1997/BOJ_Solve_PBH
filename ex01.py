03b54363c424630d2074ec54a3a22b5f77de8bf3
park1997 <iambyunghyun97@gmail.com>
2021-05-27 15:55:40 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,3 +1,9 @@
-a= int(input())
-shape = [list(map(int,input().split())) for i in range(a)]
-
+a= 1
+while 1:
+    if a*(a+1)/2 >=3000:
+        result = a*(a+1)//2
+        loc = a
+        break
+    a+=1
+print("1~1000의 합에서 최초로 3000이 넘는 위치 :",loc)
+print("숫자의 합은 :",result)
