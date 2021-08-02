817fa2bf6636b343ab4bd7a21e1b2d5146c54667
park1997 <iambyunghyun97@gmail.com>
2021-08-02 17:01:28 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,6 +1,4 @@
-inStr = "파이썬 기말고사 주의사항 : 답안지 스캔 주의, 촬영 주의. 기말고사 시간 엄수. 파이썬 교재및 강의자료 답안작성 활용 가능"
+map = [[j for j in input()] for i in range(int(input()))]
+print(map)
 
-file_in = open("notice.txt",mode = 'rwt',encoding='utf-8')
-inStr = file_in.readlines()
 
-print(inStr)
