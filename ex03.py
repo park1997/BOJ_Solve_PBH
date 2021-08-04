fe16ea16618adb8b8dc23565cac392f441a15d42
park1997 <iambyunghyun97@gmail.com>
2021-08-04 16:38:22 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,20 +1,36 @@
-def my_Func(start, end=0, hop =1):
-    if start > end :
-        init_val = 0
-        while init_val < start+1:
-            yield init_val
-            init_val += hop
-    else:
-        init_val = start
-        while init_val < end +1:
-            yield init_val
-            init_val += hop 
+num = str(input())
+left_len = 1
+right_len = 1
+cnt = 0
+if len(num) == 1:
+    print(num,num)
+else:
+    while 1:
+        # print(num,left_len,right_len)
+        if (len(str(left_len))**10 - int(num[:left_len]))*len(str(left_len)) + (len(str(right_len))**10-int(num[-right_len:]))*len(str(right_len)) == len(num):
+            break
+        elif (left_len+right_len)%2 == 0:
+            right_len += 1
+        elif (left_len+right_len)%2 == 1:
+            left_len += 1
+        print(left_len,right_len)
+        # else:
+        #     result = ""
+        #     if int(num[:left_len]) < int(num[-right_len:]):
+        #         for i in range(int(num[:left_len]),int(num[-right_len:])+1):
+        #             result += str(i)
+        #             if result[:len(result)] != num[:len(result)] :
+        #                 break
+        #         if result == num:
+        #             break
+        #         else:
+        #             left_len+=1
+        #     else:
+        #         right_len+=1
+        #     if left_len == len(num) and right_len==len(num):
+        #         break
+    print(num[:left_len],num[-right_len:])
 
-for i in my_Func(5):
-    print(i)
-print()
-for i in my_Func(1,5):
-    print(i)
-print()
-for i in my_Func(1,5,2):
-    print(i)
+21222324
+
+100 - 21 + 100 - 
