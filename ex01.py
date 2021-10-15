968dfc28fb13c13a2b85015dd7ad2f8b6957e527
park1997 <iambyunghyun97@gmail.com>
2021-10-15 15:03:03 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,5 +1,15 @@
-a = list(map(int,input().split()))
-b = [i*2 for i in a]
-for i in b:
-    a.append(i)
-print(*b)
+T = int(input())
+for i in range(T):
+    n = int(input())
+    sticker = [list(map(int,input().split())) for j in range(2)]
+    if n==1:
+        print(max(sticker[0][0],sticker[1][0]))
+        continue
+    for k in range(1,n):
+        if k == 1:
+            sticker[0][1] += sticker[1][0]
+            sticker[1][1] += sticker[0][0]
+        else:
+            sticker[0][k] += max(sticker[1][k-1],sticker[1][k-2])
+            sticker[1][k] += max(sticker[0][k-1],sticker[0][k-2])
+    print(max(sticker[0][-1],sticker[1][-1]))
