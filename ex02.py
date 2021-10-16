078b0a6efa5e8ecca23342a8627298b5a35030d7
park1997 <iambyunghyun97@gmail.com>
2021-10-16 19:29:17 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,13 +1,12 @@
-file_in = open("notice.txt",'r',encoding='utf-8')
-inStr = file_in.readline()
-
-wordlist = list(map(str,inStr.split()))
-str_word = ['파이썬','기말','주의','답안']
-
-count_word = [0]*len(str_word)
-
-for i in range(0,len(str_word)):
-    for word in wordlist:
-        if str_word[i] in word:
-            count_word[i] +=1
-print(count_word)
+N = int(input())
+a0 = 0
+a1 = 1
+if N ==1:
+    print(1)
+else:
+    for i in range(N-1):
+        result0 = a1 + a0
+        result1 = a0
+        a0  = result0
+        a1  = result1
+    print(a0+a1)
