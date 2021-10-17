e8571ee9ef7730b179eb4c4c8456c3d2939a4d6d
park1997 <iambyunghyun97@gmail.com>
2021-10-18 06:53:42 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,19 +1,10 @@
+from math import sqrt
 N = int(input())
-if N ==1:
-    print(9)
-else:
-    dp = {}
-    for i in range(10):
-        dp[i]=0
-    for i in range(1,N):
-        for j in dp:
-            if j==0:
-                dp[1]+=1
-            elif j==9:
-                dp[8]+=1
-            else:
-                dp[j-1]+=1
-                dp[j+1]+=1
-        if i ==1:
-            dp[0] = 0
-        print(dp)
+dp = [0]*(N+1)
+dp[0] = 0
+dp[1] = 1
+for i in range(2,N+1):
+    dp[i] = i
+    for j in range(1,int(sqrt(i))+1):
+        dp[i] = min(dp[i],dp[i-j*j]+1)
+print(dp[-1])
