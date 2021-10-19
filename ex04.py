26dd832cc166a0cf7c8cd272104d03508da25c52
park1997 <iambyunghyun97@gmail.com>
2021-10-19 11:10:57 +0900

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,14 +1,7 @@
 N,M = map(int,input().split())
 data = [list(map(int,input().split())) for i in range(N)]
-dp = [[0]*M]*N
-if N ==1:
-    print(data[-1][-1])
-else:
-    for i in range(0,N):
-        dp[0] = data[0]
-        for j in range(M):
-            if j==0:
-                dp[i][j] = dp[i-1][j]+data[i][j]
-            else:
-                dp[i][j] = max(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+data[i][j]
-    print(dp[-1][-1])
+dp = [[0]*(M+1)]*(N+1)
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        dp[i][j] = max(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+data[i-1][j-1]
+print(dp[-1][-1])
