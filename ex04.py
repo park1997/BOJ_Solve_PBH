81de7250c5a2d93578e5e85a9271990a2811381c
park1997 <iambyunghyun97@gmail.com>
2021-10-26 17:27:33 +0900

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,7 +1,31 @@
-N,M = map(int,input().split())
-data = [list(map(int,input().split())) for i in range(N)]
-dp = [[0]*(M+1)]*(N+1)
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        dp[i][j] = max(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+data[i-1][j-1]
-print(dp[-1][-1])
+width, height = 10, 10
+row, col, color = 5, 5, 1
+map_ = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
+queue = []
+
+
+def bfs(coord, color):
+    global map_, queue
+    queue.append(coord)
+    map_[coord[0]][coord[1]] = color
+    while len(queue) != 0:
+        front = queue.pop(0)
+        for (drow, dcol) in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+            if (0 <= front[0]+drow < height and 0 <= front[1]+dcol < width) and map_[front[0]+drow][front[1]+dcol] == 0:
+                queue.append((front[0]+drow, front[1]+dcol))
+                map_[front[0]+drow][front[1]+dcol] = color
+
+bfs((row, col), color)
+for i in range(height):
+    for j in range(width):
+        print(map_[i][j], end='')
+    print('')
