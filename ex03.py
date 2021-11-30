57a50ea18b29f3b045466087c033e3f22f7b2cfc
park1997 <iambyunghyun97@gmail.com>
2021-11-30 20:05:50 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,10 +1,10 @@
-from math import sqrt
 N = int(input())
-dp = [0]*(N+1)
-dp[0] = 0
-dp[1] = 1
-for i in range(2,N+1):
-    dp[i] = i
-    for j in range(1,int(sqrt(i))+1):
-        dp[i] = min(dp[i],dp[i-j*j]+1)
-print(dp[-1])
+data = [int(input()) for i in range(N)]
+data.sort()
+result = 0
+result = data[0]*N
+for i in range(1,len(data)):
+    temp = data[i]*(N-i)
+    if result<temp:
+        result = temp
+print(result)
