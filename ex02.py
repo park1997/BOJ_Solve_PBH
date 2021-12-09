38ae4bb7d946b039ce1ae478ff71527c22a07b7d
park1997 <iambyunghyun97@gmail.com>
2021-12-09 20:31:27 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,6 +1,4 @@
-result=[1,2,1,1,1,1,2]
-n=int(input())
-if result[(n%7)-1] == 1:
-    print("SK")
-else:
-    print("CY")
+import pandas as pd
+dic = {"마력":[130,250,190,300,210],"총중량":[1900,2600,2200,2900,2400],"연비":[16.3,10.2,11.1,7.1,12.1]}
+df = pd.DataFrame(dic,columns=["A","B","C","D","E"])
+print(df)
