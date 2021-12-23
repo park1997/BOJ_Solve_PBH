3981aac6f65b8a340331918da535f447d397debe
park1997 <iambyunghyun97@gmail.com>
2021-12-23 16:34:22 +0900

    Update 음료수얼려먹기.py

--- Algorithm/나동빈_취업을위한코딩테스트/DFS,BFS/음료수얼려먹기.py
+++ Algorithm/나동빈_취업을위한코딩테스트/DFS,BFS/음료수얼려먹기.py
@@ -5,6 +5,25 @@
 11111
 00000
 """
+def dfs(x,y):
+    if x<=-1 or x>=N or y<=-1 or y>=M:
+        return False
+    if graph[x][y]==0:
+        graph[x][y]=1
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        return True
+    return False
+
+
 N,M = map(int,input().split())
-graph = [list(input()) for i in range(N)]
-print(graph)
+graph = [list(map(int,input())) for i in range(N)]
+cnt=0
+for i in range(N):
+    for j in range(M):
+        if dfs(i,j):
+            cnt+=1
+print(cnt)
+
