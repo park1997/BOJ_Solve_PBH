cf854cd457411c0481671c647f63fd1f631bbfe7
park1997 <iambyunghyun97@gmail.com>
2022-01-05 17:03:07 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,37 +1,12 @@
-def iscross(v,i,j):
-    return  (v[i][0] < v[j][0] and v[i][1] > v[j][1]) or (v[i][0] > v[j][0] and v[i][1] < v[j][1])
-
 N = int(input())
-v = [list(map(int,input().split())) for i in range(N)]
-v_dic = {}
-for i in range(N):
-    cnt = 0
-    for j in range(N):
-        if i==j:
-            continue
-        if iscross(v,i,j):
-            cnt+=1
-    v_dic[i] = cnt
-v_dic = sorted(v_dic.items(), key = lambda x : x[1], reverse=True)
-print(v_dic)
-print(v)
-result = 0
-for _ in range(N):
-    flag = False
-    v[v_dic[_][0]] = [-1,-1]
-    print(v)
-    for i in range(_,N):
-        cnt = 0
-        for j in range(_,N):
-            if i == j : continue
-            if iscross(v,i,j):
-                flag = True
-                cnt += 1
-                break
-        if flag:
-            break
-    if cnt == 0:
-        result = _+1
-        break
+data = [list(map(int,input().split())) for i in range(N)]
+data.sort(key=lambda x: (x[1],x[0]))    # 가장 먼저 끝나느 회의 순으로 회의를 정렬
+result = 1
+s = data[0][0]  # start 포인트, 회의의 시작시간
+e = data[0][1]  # end 포인트, 회의의 끝나는 시간
+for i in range(1,len(data)):    # 가장 먼저 끝나느 회의중 첫번째 회의는 무조건 진행하므로 for loop는  1부터 탐색 시작
+    if data[i][0]>=e: # 만약에 현재 회의의 끝나는 시간보다 크거나 같은 시작시간을 가지고있는 회의를 발견할 시 그 회의 선택 
+        s = data[i][0]  # 회의의 start시간 update
+        e = data[i][1]  # 회의의 end시간 update
+        result+=1   # 결과 +=1
 print(result)
-
