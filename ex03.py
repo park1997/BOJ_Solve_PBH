3f4a0be97b3fbba2bca29f197d1c2f91d40d9416
park1997 <iambyunghyun97@gmail.com>
2022-01-05 17:03:11 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,10 +1,20 @@
+from collections import deque
 N = int(input())
-data = [int(input()) for i in range(N)]
-data.sort()
-result = 0
-result = data[0]*N
-for i in range(1,len(data)):
-    temp = data[i]*(N-i)
-    if result<temp:
-        result = temp
-print(result)
+c = int(input())
+graph = [[] for _ in range(N+1)]
+visited = [False]*(N+1)
+for i in range(c):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(start):
+    q = deque([start])
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+    print(sum(visited)-1)
+bfs(1)
