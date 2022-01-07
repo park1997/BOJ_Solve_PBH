80b4b1cc2382cf40bc5c77bc7d336da7e8e8c998
park1997 <iambyunghyun97@gmail.com>
2022-01-07 13:58:07 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,20 +1,23 @@
-from collections import deque
-N = int(input())
-c = int(input())
+import sys
+sys.setrecursionlimit(10**9)
+def dfs(start):
+    global visited
+    global graph
+    visited[start] = True
+    for k in graph[start]:
+        if not visited[k]:
+            dfs(k)
+
+N,M = map(int,sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
 visited = [False]*(N+1)
-for i in range(c):
-    a,b = map(int,input().split())
+for i in range(M):
+    a,b = map(int,sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
-
-def bfs(start):
-    q = deque([start])
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
-    print(sum(visited)-1)
-bfs(1)
+cnt = 0
+for i in range(1,N+1):
+    if not visited[i]:
+        dfs(i)
+        cnt +=1
+print(cnt)
