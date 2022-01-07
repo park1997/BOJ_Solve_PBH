c3406968b484b041e9c949ac0e346163f4d2cd96
park1997 <iambyunghyun97@gmail.com>
2022-01-07 13:58:03 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,12 +1,28 @@
-N = int(input())
-data = [list(map(int,input().split())) for i in range(N)]
-data.sort(key=lambda x: (x[1],x[0]))    # 가장 먼저 끝나느 회의 순으로 회의를 정렬
-result = 1
-s = data[0][0]  # start 포인트, 회의의 시작시간
-e = data[0][1]  # end 포인트, 회의의 끝나는 시간
-for i in range(1,len(data)):    # 가장 먼저 끝나느 회의중 첫번째 회의는 무조건 진행하므로 for loop는  1부터 탐색 시작
-    if data[i][0]>=e: # 만약에 현재 회의의 끝나는 시간보다 크거나 같은 시작시간을 가지고있는 회의를 발견할 시 그 회의 선택 
-        s = data[i][0]  # 회의의 start시간 update
-        e = data[i][1]  # 회의의 end시간 update
-        result+=1   # 결과 +=1
-print(result)
+dx = [1, -1, 0, 0, 1, -1, 1, -1]
+dy = [0, 0, -1, 1, -1, -1, 1, 1]
+def bfs(i, j):
+    s[i][j] = 0
+    queue = [[i, j]]
+    while queue:
+        a, b = queue[0][0], queue[0][1]
+        del queue[0]
+        for k in range(8):
+            x = a + dx[k]
+            y = b + dy[k]
+            if 0 <= x < h and 0 <= y < w and s[x][y] == 1:
+                s[x][y] = 0
+                queue.append([x, y])
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    s = []
+    cnt = 0
+    for i in range(h):
+        s.append(list(map(int, input().split())))
+    for i in range(h):
+        for j in range(w):
+            if s[i][j] == 1:
+                bfs(i, j)
+                cnt += 1
+    print(cnt)
