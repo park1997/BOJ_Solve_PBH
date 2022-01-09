41cddd210d77a7e4ad031aee4aeffb399c21dfbc
park1997 <iambyunghyun97@gmail.com>
2022-01-09 17:22:04 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,23 +1,6 @@
-import sys
-sys.setrecursionlimit(10**9)
-def dfs(start):
-    global visited
-    global graph
-    visited[start] = True
-    for k in graph[start]:
-        if not visited[k]:
-            dfs(k)
-
-N,M = map(int,sys.stdin.readline().split())
-graph = [[] for _ in range(N+1)]
-visited = [False]*(N+1)
-for i in range(M):
-    a,b = map(int,sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-cnt = 0
-for i in range(1,N+1):
-    if not visited[i]:
-        dfs(i)
-        cnt +=1
-print(cnt)
+D,H,W = map(int,input().split())
+R = min(H,W)
+H /= R
+W /= R
+a = (D**2//(H**2+W**2))**(1/2)
+print(int(H*a),int(W*a))
