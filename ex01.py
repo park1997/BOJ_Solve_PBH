0fa23a3820c4f7602f5820f6c90f0105fe563802
park1997 <iambyunghyun97@gmail.com>
2022-01-10 22:02:04 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,33 +1,17 @@
-from collections import deque
-
-N = int(input())
-graph = [list(map(int,input())) for i in range(N)]
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
-visited = [[False]*N for _ in range(N)]
-q = deque()
-result = []
+import sys
+N = int(sys.stdin.readline())
+num = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+dp = [0]*(N)
 for i in range(N):
-    for j in range(N):
-        cnt = 1
-        # print(i,j)
-        if graph[i][j]==1 and not visited[i][j]:
-            q.append([i,j])
-            visited[i][j] = True
-            while q:
-                # print(q)
-                x,y = q.popleft()
-                for d in range(4):
-                    nx = x + dx[d]
-                    ny = y + dy[d]
-                    if nx>=0 and ny>=0 and ny<N and nx<N and graph[nx][ny]==1 and not visited[nx][ny]:
-                        graph[nx][ny]=0
-                        q.append([nx,ny])
-                        cnt+=1
-            result.append(cnt)
-# print()
-print(len(result))
-result.sort()
-for i in result:
-    print(i)
-
+    if num[i][0] + i <= N:
+        dp[i] = num[i][1]
+        for j in range(i):
+            if i >= j + num[j][0]:
+                dp[i] = max(dp[j],num[i][1]+dp[j])
+    else:
+        dp[i] = 0
+        for j in range(i):
+            if i >= j+num[j][0]:
+                dp[i] = max(dp[j],dp[i])
+print(max(dp))
+print(dp)
