2dc6504722cbefc22e05667a78871173eac60f79
park1997 <iambyunghyun97@gmail.com>
2022-01-13 02:33:14 +0900

    Update ex04.py

--- ex04.py
+++
@@ -1,7 +0,0 @@
-import sys
-from collections import deque
-N,M = map(int,sys.stdin.readline().split())
-graph = [list(sys.stdin.readline().strip()) for _ in range(N)]
-
-for g in graph:
-    print(g)
