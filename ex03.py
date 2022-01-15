8681bd10ab14a77282906e51460dc0c54271c934
park1997 <iambyunghyun97@gmail.com>
2022-01-15 18:06:52 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,45 +1,45 @@
-from collections import deque
-from itertools import combinations
 import sys
-def bfs(tg,x,y,num):
-    q = deque()
-    q.append([x,y])
-    tg[x][y] = num
+from collections import deque
+def bfs(x,y):
+    global graph,N,M
+    t_graph = [g[:] for g in graph]
+    visited = [[False]*M for _ in range(N)]
     dx = [0,1,0,-1]
     dy = [1,0,-1,0]
+    q = deque()
+    q.append([x,y])
+    visited[x][y] = True
     while q:
         a,b = q.popleft()
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
             if nx>=0 and ny>=0 and nx<N and ny<M:
-                if tg[nx][ny]==0:
+                if t_graph[nx][ny] == 0 and not visited[nx][ny]:
+                    t_graph[nx][ny] = t_graph[a][b] + 1
+                    visited[nx][ny] = True
                     q.append([nx,ny])
-                    tg[nx][ny] = num
+    t_max = -1
+    for t in t_graph:
+        if t_max < max(t):
+            t_max = max(t)
+    return t_max
 
 N,M = map(int,sys.stdin.readline().split())
-graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-wall = list(combinations([i for i in range(N*M)],3))
+graph = [list(map(str,sys.stdin.readline().strip())) for _ in range(N)]
+
+for x in range(N):
+    for y in range(M):
+        if graph[x][y] == "L":
+            graph[x][y] = 0
+        elif graph[x][y] == "W":
+            graph[x][y] = -1
+
 result = -1
-for w in wall:
-    w1,w2,w3 = w
-    w1 = [w1//M,w1%M]
-    w2 = [w2//M,w2%M]
-    w3 = [w3//M,w3%M]
-    if graph[w1[0]][w1[1]]==0 and graph[w2[0]][w2[1]]==0 and graph[w3[0]][w3[1]]==0:
-        tg = [i[:] for i in graph]
-        tg[w1[0]][w1[1]] = 1
-        tg[w2[0]][w2[1]] = 1
-        tg[w3[0]][w3[1]] = 1
-        for x in range(N):
-            for y in range(M):
-                if tg[x][y] == 2:
-                    bfs(tg,x,y,2)
-        cnt = 0
-        for x in range(N):
-            for y in range(M):
-                if tg[x][y] == 0:
-                    cnt += 1
-        if cnt > result:
-            result = cnt
+for x in range(N):
+    for y in range(M):
+        if graph[x][y] == 0:
+            temp = bfs(x,y)
+            if result < temp:
+                result = temp
 print(result)
