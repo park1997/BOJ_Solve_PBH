b04c3cd8449fcde7a32ea874ef285e02bf689571
park1997 <iambyunghyun97@gmail.com>
2022-01-22 21:56:46 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,53 +1,42 @@
-import sys
-from collections import deque
-def bfs(start):
-    global graph,R,C,dx,dy
-    q = deque()
-    q.append(start)
-    visited2 = [[False]*C for _ in range(R)]
-    visited2[start[0]][start[1]] = True
-    while q:
-        a,b = q.popleft()
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if nx>=0 and ny>=0 and nx<R and ny<C:
-                if graph[nx][ny] == "." and not visited2[nx][ny]:
-                    q.append([nx,ny])
-                    visited2[nx][ny] = True
-                if graph[nx][ny] == "L" and not visited2[nx][ny]:
-                    return True
-    return False 
-R,C = map(int,sys.stdin.readline().split())
-graph = []
-L = []
-for i in range(R):
-    a = list(sys.stdin.readline().strip())
-    for j in range(len(a)):
-        if "L" == a[j]:
-            L.append([i,j])
-            break
-    graph.append(a)
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-
-cnt =0
-while 1:
-    if bfs(L[0]):
-        break
+def dfs(s):
+    global r,l,visited, graph,cnt
+    cnt += 1
+    for i in graph[s]:
+        if not visited[i]:
+            visited[i] = True
+            if cnt%2==0:
+                if i not in r:
+                    r.append(i)
+                    dfs(i)
+                else:
+                    return False
+            else:
+                if i not in l:
+                    l.append(i)
+                    dfs(i)
+                else:
+                    return False
+    return True
+V,E = map(int,input().split())
+graph = [[] for _ in range(V+1)]
+for i in range(E):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+flag = False
+for i in range(1,V):
+    r = []
+    l = []
+    visited = [False]*(V+1)
+    cnt = 0
+    if dfs(i):
+        print(1)
     else:
-        cnt += 1
-        visited = [[False]*C for _ in range(R)]
-        for x in range(R):
-            for y in range(C):
-                if graph[x][y] == "." and not visited[x][y]:
-                    visited[x][y] = True
-                    for i in range(4):
-                        nx = x + dx[i]
-                        ny = y + dy[i]
-                        if nx>=0 and ny>=0 and nx<R and ny<C:
-                            if graph[nx][ny] == "X":
-                                graph[nx][ny] = "."
-                                visited[nx][ny] = True
-print(cnt)
+        print(2)
+        flag = True
+if flag:
+    print("NO")
+else:
+    print("YES")
+
 
