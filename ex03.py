c54eaeec6fbd2cb8a65ded69a3fb0cc24e7ecd37
park1997 <iambyunghyun97@gmail.com>
2022-01-25 21:45:19 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,42 +1,20 @@
-def dfs(s):
-    global r,l,visited, graph,cnt
-    cnt += 1
-    for i in graph[s]:
-        if not visited[i]:
-            visited[i] = True
-            if cnt%2==0:
-                if i not in r:
-                    r.append(i)
-                    dfs(i)
-                else:
-                    return False
-            else:
-                if i not in l:
-                    l.append(i)
-                    dfs(i)
-                else:
-                    return False
-    return True
-V,E = map(int,input().split())
-graph = [[] for _ in range(V+1)]
-for i in range(E):
-    a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-flag = False
-for i in range(1,V):
-    r = []
-    l = []
-    visited = [False]*(V+1)
-    cnt = 0
-    if dfs(i):
-        print(1)
-    else:
-        print(2)
-        flag = True
-if flag:
-    print("NO")
-else:
-    print("YES")
-
+N = int(input())
+num = list(map(int,input().split()))
+M = int(input())
+start = 0
+end = 0
+result = num[start]
+cnt = 0
+while start<=end:
+    try:
+        if result<=M:
+            end+=1
+            result += num[end]
+        elif result>M:
+            result-=num[start]
+            start+=1
+            cnt += N-end
+    except:
+        break
+print(cnt)
 
