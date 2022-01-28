2803dd3a83c3467b21d291f16ed8108ba5afdac6
park1997 <iambyunghyun97@gmail.com>
2022-01-28 17:24:57 +0900

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,38 +1,41 @@
 from collections import deque
 import sys
-def bfs(s):
-    global visited
+
+def bfs(x,y,z):
+    global graph, visited,N,M,flag
     q = deque()
-    q.append(s)
-    visited[s] = 1
-    while q:
-        a = q.popleft()
-        for i in graph[a]:
-            if visited[i]==0:
-                q.append(i)
-                visited[i] = -visited[a]
-            else:
-                if visited[i]==visited[a]:
-                    return False
-    return True
+    q.append([x,y,z])
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    visited[z][x][y] = 1
+    while q and flag:
+        a,b,c = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<N and ny<M:
+                if visited[c][nx][ny] == 0 and graph[nx][ny] == 1 and c == 0:
+                    q.append([nx,ny,1])
+                    visited[1][nx][ny] =  visited[c][a][b] + 1
+                elif visited[c][nx][ny] == 0 and graph[nx][ny] == 0:
+                    q.append([nx,ny,c])
+                    visited[c][nx][ny] =  visited[c][a][b] + 1
+            
+                if nx == N-1 and ny == M-1:
+                    print(visited[c][nx][ny])
+                    flag = False
+                    break
 
-K = int(input())
-for i in range(K):
-    V,E = map(int,sys.stdin.readline().split())
-    graph = [[] for _ in range(V+1)]
-    visited = [0]*(V+1)
-    for _ in range(E):
-        a,b = map(int,sys.stdin.readline().split())
-        graph[a].append(b)
-        graph[b].append(a)
+
+
+N,M = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
+visited = [[[0]*M for _ in range(N)] for i in range(2)]
+flag = True
+if N==1 and M ==1 and graph[0][0] == 0:
     flag = False
-    for i in range(1,V+1):
-        if visited[i]==0:
-            if not bfs(i):
-                flag = True
-                break
-
-    if flag:
-        print("NO")
-    else:
-        print("YES")
+    print(1)
+else:
+    bfs(0,0,0)
+if flag:
+    print(-1)
