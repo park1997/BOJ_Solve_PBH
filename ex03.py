eb2560525916a65ba120fe8841715cc17e067ea7
park1997 <iambyunghyun97@gmail.com>
2022-02-02 16:02:51 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,11 +1,28 @@
 import sys
-N = int(sys.stdin.readline())
-a = [0]*(10001)
-for i in range(N):
-    a[int(sys.stdin.readline())]+=1
+from collections import deque
 
-for i in range(len(a)):
-    if a[i]!=0:
-        for j in range(a[i]):
-            print(i)
-    
+def bfs(x,y):
+    global visited
+    q = deque()
+    q.append([x])
+    dx = [2,-1,1]
+    visited[x] = 1
+    while q:
+        a = q.popleft()[0]
+        for i in range(3):
+            if i==0:
+                nx = a * dx[i]
+            else:
+                nx = a + dx[i]
+            if nx>=0 and nx<len(visited) and visited[nx] == 0:
+                visited[nx] =  visited[a] + 1
+                q.append([nx])
+
+X,Y = map(int,sys.stdin.readline().split())
+if X<Y:
+    visited = [0]*(max(X,Y)+1)
+    bfs(X,Y)
+    print(visited[Y]-1)
+    # print(visited)
+else:
+    print(X-Y)
