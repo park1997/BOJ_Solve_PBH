e4f10072c6a60032d769ab1b29137e3d7132747a
park1997 <iambyunghyun97@gmail.com>
2022-02-10 07:50:36 +0900

    Ex

--- ex03.py
+++ ex03.py
@@ -1,9 +1,67 @@
-n,k = map(int,input().split())
-coin = [int(input()) for _ in range(n)]
-dp = [0]*(k+1)
-dp[0]=1
-for c in coin:
-    for i in range(c,k+1):
-        if i>=c:
-            dp[i] = dp[i] + dp[i-c]
-print(dp[-1])
+import sys
+from collections import deque
+
+def bfs(water):
+    q = deque(water)
+    w = []
+    ice = []
+    while q:
+        a,b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<N and ny<M:
+                if graph[nx][ny] != 0:
+                    if [a,b] not in w:
+                        w.append([a,b])
+                    graph[nx][ny]-=1
+                    if graph[nx][ny] == 0 and [nx,ny] not in w:
+                        w.append([nx,ny])
+
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j]!=0:
+                ice.append([i,j])
+    return w,ice
+
+def check_split(ice):
+    visited = [[False]*M for _ in range(N)]
+    q = deque()
+    q.append(ice[0])
+    visited[ice[0][0]][ice[0][1]] = True
+    while q:
+        a,b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<N and ny<M:
+                if not visited[nx][ny] and graph[nx][ny]!=0:
+                    visited[nx][ny] = True
+                    q.append([nx,ny])
+
+    for i in ice:
+        if not visited[i[0]][i[1]]:
+            return True
+    return False
+
+
+N,M = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+water = []
+for i in range(N):
+    for j in range(M):
+        if graph[i][j]==0:
+            water.append([i,j])
+cnt = 0
+while 1:
+    cnt += 1
+    water,ice = bfs(water)
+    if len(ice)!=0:
+        if check_split(ice):
+            print(cnt)
+            break
+    else:
+        print(0)
+        break
--- ex04.py
+++ ex04.py
@@ -1,14 +1,73 @@
-def DFS(numbers, idx, value, target):
-    result = 0
-    if idx == len(numbers) :
-        if value == target:
-            return 1
-        else:
-            return 0
-    result += DFS(numbers, idx+1, value + numbers[idx], target)
-    result += DFS(numbers, idx+1, value - numbers[idx], target)
-    return result
+from collections import deque
+import sys
+N = int(sys.stdin.readline())
+def bfs(start,type):
+    global graph, f_visited, w_visited
+    dx = [0,1,-1,0]
+    dy = [1,0,0,-1]
+    q = deque(start)
+    if type == "f":
+        for i in start:
+            f_visited[i[0]][i[1]] = 1
+    elif type == "w":
+        for i in start:
+            w_visited[i[0]][i[1]] = 1
+    while q:
+        a,b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<h and ny<w :
+                if type == 'f':
+                    if graph[nx][ny] != "#" and f_visited[nx][ny] == 0:
+                        f_visited[nx][ny] = f_visited[a][b] + 1
+                        q.append([nx,ny])
+                elif type == "w":
+                    if graph[nx][ny] != "#" and w_visited[nx][ny] == 0 and ((f_visited[nx][ny] == 0) or (f_visited[nx][ny] > w_visited[a][b] + 1)):
+                        w_visited[nx][ny] = w_visited[a][b] + 1
+                        q.append([nx,ny])
 
-def solution(numbers, target):
-    answer = DFS(numbers, 0, 0 , target)
-    return answer
+for _ in range(N):
+    w,h = map(int,sys.stdin.readline().split())
+    graph = [list(map(str,sys.stdin.readline().strip())) for _ in range(h)]
+    f_visited = [[0]*w for _ in range(h)]
+    w_visited = [[0]*w for _ in range(h)]
+    s = []
+    fire = []
+    gate = []
+    result = []
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == "*":
+                fire.append([i,j])
+            elif graph[i][j] == "@":
+                s.append([i,j])
+            if (i==0 or i==h-1 or j ==0 or j==w-1) and (graph[i][j]=="." or graph[i][j]=="@"):
+                gate.append([i,j])
+    # print(gate)
+    bfs(fire,"f")
+    bfs(s,"w")
+    # for f in f_visited:
+    #     print(f)
+    # print()
+    # for w in w_visited:
+    #     print(w)
+    for g in gate:
+        if w_visited[g[0]][g[1]]!=0:
+            result.append(w_visited[g[0]][g[1]])
+    if len(result)!=0:
+        print(min(result))
+    else:
+        print('IMPOSSIBLE')
+
+
+'''
+1
+7 6
+###.###
+###.###
+#.....#
+#.....#
+#..@..#
+#######
+'''
