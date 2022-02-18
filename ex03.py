b182bef792047bdd4079a1aae0339b67612c1f8f
park1997 <iambyunghyun97@gmail.com>
2022-02-18 22:54:56 +0900

    ex

--- ex03.py
+++ ex03.py
@@ -1,69 +1,30 @@
-from collections import deque
+import sys
+import heapq
+def bfs(K):
+    q = []
+    heapq.heappush(q,(0,K))
+    distance[K] = 0
+    while q:
+        dis, now = heapq.heappop(q)
+        if distance[now] < dis:
+            continue
+        for v1,w1 in graph[now]:
+            if distance[v1] > w1 + distance[now]:
+                distance[v1] = w1 + distance[now]
+                heapq.heappush(q,(distance[v1],v1))
 
-n, m, energy = map(int, input().split(' '))
-graph = [list(map(int, input().split(' '))) for _ in range(n)]
-sx, sy = map(int, input().split(' '))
-people = [list(map(int, input().split(' '))) for _ in range(m)]
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-def bfs(sx, sy):
-    visited = [[-1] * n for _ in range(n)]
-    queue = deque()
-    queue.append((sx, sy))
-    visited[sx][sy] = 0
-    while queue:
-        a, b = queue.popleft()
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if nx >= 0 and nx < n and ny >= 0 and ny < n:
-                if graph[nx][ny] != 1 and visited[nx][ny] == -1:
-                    visited[nx][ny] = visited[a][b] + 1
-                    queue.append([nx, ny])
-    return visited
-
-def check_dist(visited, people):
-    
-    for idx,p in enumerate(people):
-        px,py,ax,ay = p
-        people[idx].append(visited[px - 1][py - 1])
-    print(people)
-    people.sort(key=lambda x: (-x[4], -x[0], -x[1]))
-    print(people)
-
-def solve(sx, sy):
-    global energy
-    while people:
-        visitied = bfs(sx - 1, sy - 1)
-        check_dist(visitied, people)
-        p_x, p_y, ax, ay, dist = people.pop()
-        
-        for temp in people:
-            temp.pop()
-        
-        visitied = bfs(p_x - 1, p_y - 1)
-        dist2 = visitied[ax - 1][ay - 1]
-        sx, sy = ax, ay
-
-        if dist == -1 or dist2 == -1:
-            print(-1)
-            return
-
-        energy -= dist
-        if energy < 0:
-            break
-
-        energy -= dist2
-        if energy < 0:
-            break
-
-        energy += dist2 * 2
-
-    if energy < 0:
-        print(-1)
+V,E = map(int,sys.stdin.readline().split())
+K = int(sys.stdin.readline())
+distance = [int(1e9)]*(V+1)
+visited = [False]*(V+1)
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    u,v,w = map(int,sys.stdin.readline().split())
+    # u노드에서 v노드까지 가는비용 w
+    graph[u].append([v,w])
+bfs(K)
+for i in range(1,V+1):
+    if distance[i] == int(1e9):
+        print("INF")
     else:
-        print(energy)
-
-
-solve(sx, sy)
+        print(distance[i])
