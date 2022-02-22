979a01f94f37572419b9a9593948b8edb3fa4f7f
park1997 <iambyunghyun97@gmail.com>
2022-02-23 01:32:25 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,30 +1,44 @@
 import sys
-import heapq
-def bfs(K):
-    q = []
-    heapq.heappush(q,(0,K))
-    distance[K] = 0
+from collections import deque
+def bfs(start):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    q = deque([start])
     while q:
-        dis, now = heapq.heappop(q)
-        if distance[now] < dis:
-            continue
-        for v1,w1 in graph[now]:
-            if distance[v1] > w1 + distance[now]:
-                distance[v1] = w1 + distance[now]
-                heapq.heappush(q,(distance[v1],v1))
+        a,b,c = q.popleft()
+        if [a,b] == [Ex-1,Ey-1]:
+            print(c,a,b)
+            return visited[c][a][b]
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<N and ny<M and not visited[c][nx][ny]:
+                if graph[nx][ny] == 0 and c == 0:
+                    q.append([nx,ny,0])
+                    visited[0][nx][ny] = visited[c][a][b] + 1
+                elif graph[nx][ny] == 0 and c == 1:
+                    q.append([nx,ny,1])
+                    visited[1][nx][ny] = visited[c][a][b] + 1
+                elif graph[nx][ny] == 1 and c == 0:
+                    q.append([nx,ny,1])
+                    visited[1][nx][ny] = visited[c][a][b] + 1
+    return -1
 
-V,E = map(int,sys.stdin.readline().split())
-K = int(sys.stdin.readline())
-distance = [int(1e9)]*(V+1)
-visited = [False]*(V+1)
-graph = [[] for _ in range(V+1)]
-for _ in range(E):
-    u,v,w = map(int,sys.stdin.readline().split())
-    # u노드에서 v노드까지 가는비용 w
-    graph[u].append([v,w])
-bfs(K)
-for i in range(1,V+1):
-    if distance[i] == int(1e9):
-        print("INF")
-    else:
-        print(distance[i])
+
+N,M = map(int,sys.stdin.readline().split())
+Hx,Hy = map(int,sys.stdin.readline().split())
+Ex,Ey = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+visited = [[[0]*M for _ in range(N)] for i in range(2)]
+print(bfs([Hx-1,Hy-1,0]))
+
+'''
+5 6
+1 1
+5 6
+0 1 1 1 0 0
+0 1 1 0 1 0
+0 1 0 0 1 0
+0 1 0 0 1 0
+0 1 0 1 1 0
+'''
