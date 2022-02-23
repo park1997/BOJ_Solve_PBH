bd2c8c437809e9e00a8718c28ccbb0fc805e7921
park1997 <iambyunghyun97@gmail.com>
2022-02-24 03:29:31 +0900

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,40 +1,30 @@
 import sys
 from collections import deque
-def bfs(start):
-    q = deque([start])
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    visited[start[0]][start[1]] = 0
+def bfs(s):
+    global cnt
+    dx = [2,1,-1]
+    visited[s] = 0
+    q = deque()
+    q.append(s)
     while q:
-        a,b = q.popleft()
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            while True:
-                if not (nx >=0 and ny>=0 and nx<H and ny<W):
-                    break
-                if visited[nx][ny] < visited[a][b] + 1:
-                    break
-                if graph[nx][ny] == "*":
-                    break
-                q.append([nx,ny])
-                visited[nx][ny] = visited[a][b]+1
-                nx = nx + dx[i]
-                ny = ny + dy[i]
-
-
-    return
-W,H = map(int,sys.stdin.readline().split())
-visited = [[int(1e9)]*W for _ in range(H)]
-graph = []
-c = []
-for i in range(H):
-    a = list(map(str,sys.stdin.readline().strip()))
-    for j in range(W):
-        if a[j] == "C":
-            c.append([i,j])
-    graph.append(a)
-
-bfs(c[0])
-print(visited[c[1][0]][c[1][1]]-1)
-
+        a = q.popleft()
+        if a == K:
+            cnt += 1
+        for i in range(3):
+            if i ==0:
+                nx = a * dx[i]
+            else:
+                nx = a + dx[i]
+            if nx >=0 and nx <= 100000 :
+                if visited[nx] == -1 or visited[nx] >= visited[a] + 1:
+                    visited[nx] = visited[a] + 1
+                    q.append(nx)
+                
+                
+    
+N,K = map(int,sys.stdin.readline().split())
+visited = [-1]*(100001)
+cnt = 0
+bfs(N)
+print(visited[K])
+print(cnt)
