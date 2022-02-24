33d8c59ea4d13e5289032a107b6831055b45a782
park1997 <iambyunghyun97@gmail.com>
2022-02-24 18:28:36 +0900

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,30 +1,32 @@
 import sys
-from collections import deque
-def bfs(s):
-    global cnt
-    dx = [2,1,-1]
-    visited[s] = 0
-    q = deque()
-    q.append(s)
+import heapq
+def Djikstra():
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    q = []
+    heapq.heappush(q,[graph[0][0],0,0])
+    visited[0][0] = graph[0][0]
     while q:
-        a = q.popleft()
-        if a == K:
-            cnt += 1
-        for i in range(3):
-            if i ==0:
-                nx = a * dx[i]
-            else:
-                nx = a + dx[i]
-            if nx >=0 and nx <= 100000 :
-                if visited[nx] == -1 or visited[nx] >= visited[a] + 1:
-                    visited[nx] = visited[a] + 1
-                    q.append(nx)
+        dis,a,b = heapq.heappop(q)
+        if [a,b] == [T-1,T-1]:
+            return
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<T and ny<T:
+                if visited[nx][ny] > dis + graph[nx][ny]:
+                    visited[nx][ny] = dis + graph[nx][ny]
+                    heapq.heappush(q,[visited[nx][ny],nx,ny])
                 
-                
-    
-N,K = map(int,sys.stdin.readline().split())
-visited = [-1]*(100001)
-cnt = 0
-bfs(N)
-print(visited[K])
-print(cnt)
+i = 0
+while 1:
+    T = int(sys.stdin.readline())
+    if T == 0:
+        break
+    i+=1
+    graph = [list(map(int,sys.stdin.readline().split())) for _ in range(T)]
+    visited = [[int(1e9)]*T for _ in range(T)]
+    result = 0
+    Djikstra()
+    print("Problem {}: {}".format(i,visited[-1][-1]))
+
