8ff7ffc1b82ed86a5bf08c9f3de2dc3d42ab9147
park1997 <iambyunghyun97@gmail.com>
2022-03-11 13:12:19 +0900

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,16 +1,35 @@
-n,m = map(int,input().split())
-idx = max(m,n)
-dp = [float('inf')]*(100001+1)
-dp[n] = 0
-for i in range(100001):
-    t = abs(n-i)
-    dp[i] = min(dp[i],t)
-for i in range(n+1,100001):
-    if i%2==0:
-        dp[i] = min(dp[i],dp[i//2],dp[i-1]+1,dp[i+1]-1)
-    else:
-        dp[i] = min(dp[i],dp[i-1]+1,dp[i+1]-1)
+import sys
+import heapq
+def Djikstra(s):
+    global n
+    distance = [int(1e9)]*(n+1)
+    q = []
+    heapq.heappush(q,[0,s])
+    distance[s] = 0
+    while q:
+        dis,a = heapq.heappop(q)
+        for node,d in graph[a]:
+            new_dis = dis + d
+            if new_dis < distance[node]:
+                distance[node] = new_dis
+                heapq.heappush(q,[new_dis,node])
+                result[node][s] = a
 
+n,m = map(int,sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+result = [[0]*(n+1) for _ in range(n+1)]
+for i in range(m):
+    a,b,t = map(int,sys.stdin.readline().split())
+    graph[a].append([b,t])
+    graph[b].append([a,t])
 
-print(dp[:m+1])
-print(dp[m])
+for i in range(1,n+1):
+    Djikstra(i)
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j:
+            print("-",end=" ")
+        else:
+            print(result[i][j],end=" ")
+    print()
