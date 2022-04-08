9b66591860a7bbda42a66d8bb70437cad669f8bf
park1997 <iambyunghyun97@gmail.com>
2022-04-08 14:30:01 +0900

    Create 8949.py

---
+++ BOJ_8xxx/8949.py
@@ -0,0 +1,15 @@
+# 대충 더해
+a,b=map(str,input().split())
+result=''
+if len(a)==len(b):
+    for i in range(len(a)):
+        result+=str((int(a[i])+int(b[i])))
+elif len(a) < len(b):
+    result+=b[0:len(b)-len(a)]
+    for i in range(len(a)):
+        result+=str(int(a[i])+int(b[i+(len(b)-len(a))]))
+elif len(a) > len(b):
+    result+=a[0:len(a)-len(b)]
+    for i in range(len(b)):
+        result+=str(int(b[i])+int(a[i+(len(a)-len(b))]))
+print(result)
