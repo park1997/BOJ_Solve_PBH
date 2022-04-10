807b21caa78482c1a85a46453864688040765ee9
park1997 <iambyunghyun97@gmail.com>
2022-04-11 03:49:28 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,64 +1,126 @@
-print("                                                           :8DDDDDDDDDDDDDD$.                                           ")
-print("                                                      DDDNNN8~~~~~~~~~~=~7DNNDNDDDNNI                                   ")
-print("                                                  ?NNDD=~=~~~~~~~~~~~~~~~~~=~~==~=INNDNN7                               ")
-print("                                               +NDDI~~~~~~~~~~~~~~~~~~~~~~~=~~========~ODND+                            ")
-print("                                            :NND~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~============7NDN                          ")
-print("                                          $DD$~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~==============~DNN                        ")
-print("                                        $DD=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~=================NND                      ")
-print("                                       ND7~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~===================DD7                    ")
-print("                                     ~DD=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=======================8DN.                  ")
-print("                                    8DO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=========================DD                  ")
-print("                                   8N~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~=======================DN                 ")
-print("                                  NN::::::::~~~~~~~~~~~=~~~~~~~~~~~~~~~~~~~=~~========================DDO               ")
-print("                                 $D$:::::::::::::::~~~~DD~~~~~~~~~~~~~~~~~~=~~=========================DN.              ")
-print("                                 D8:::::::::::::::::::DN=::~~~~~~~~~~~~~~~~=~~======================~~:~DN              ")
-print("                                DN:::::::::::::::::::ONO::::::::::::::::::::~~~~~~~~~~~~:::::::::::::::::DN             ")
-print("                               DN::::::::::::::::::::NN.:::::::::::::::::::::::::::DN::::::::::::::::::::$DO            ")
-print("                               DD:::::::::::::::::::DNI:::::::::::::::::::::::::::::D=::::::::::::::::::::NN            ")
-print("                              NN~~~~:::::$N?:::::::.NN::::::::::::::::::::::::::::::ND.:::::::::::::::::::+N8           ")
-print("                              N7~~~~~~~~OD7::::::::~DD::::::::::::::::::::::::::::::~D$::::::::::::::::::::DN           ")
-print("                             NN~~~~~~~~IDZ~~~~~::::DN~:::::::::::::::::::::::::::::::DN::::::::::::::::::::=N~          ")
-print("                             DD~~~~~~~~NN~~~~~~~~~=NN::::::::::::::::::::::::::::::::DN:::::::::::::::~~====NN          ")
-print("                            8D~~~~~~~~ND~~~~~~~~~~~ND~~~~~~~~:::::::::::::::::::::::::N7:::~~===============NN          ")
-print("                            DD~~~~~~~ON+~~~~~~~~~~~ND~~~~~~~~~~~~~~~~~~~=+NZ==========NN====================~ND         ")
-print("               :DD7   DNDD. N8~~~~~~~NN~~~~~~~~~~DDND~~~~~~~~~~~~~~~~~~~~ND~~=========DD=====================ND         ")
-print("               N~:DDNNN .8NDN~~~~~~~$D=~~~~~~~~+ND.DD~~~~~~~~~~~~~~~~~~~=DD~~=========~D+====================DN         ")
-print("              :D     .  ..~ND~~~~~~~NN~~~~~~~+NN$..ND~~~~~~~~~~~~~~~~~~~7N=~~=========~ND=======~============ON         ")
-print("              NN       ...:N?~~~~~~~N=~~~~~NNNI.. .7D+~~~~~~~~~~~~~~~~~=8NN~~==========NN=======N============$N         ")
-print("         N  ODN       ....DN~~~~~~~DD=8NNND$..     .DD~~~=~~~~~~~~~~~~~=NNDD=~=========8D~======NN===========~N$        ")
-print("    N? =NN  ND      .....NND~~~~~~~DDNN:...        .ND=~DNN~~~~~~~~~~~~=DN.DN~=========?N+======NN============ND        ")
-print("   $D? DN    DZ    ....ND8NN~~~~~~$D                .DD~NNDD~~~~~~~~~~~~D8..DN=========~DN======NN============DN        ")
-print("   DN ~N~   NN    ..:~NN..NZ~~~~~~DN                  NNN8.ND~~~~NDN?~~~DZ...7DD=======~NN======NN============DN        ")
-print("   ND DD    :DN.  ..ND$  .N?~~~~~=NNN                   . ..DDD$~N8OND8=N+   ..DDDZ~====NN======+D+===========ND        ")
-print("   NO         DD  ZDN    8NO~~~~~~NNN..DDDNN7               ...NND...:DDD:     .:.NDND=~DD======~DO===========DN        ")
-print("              DNDDN:.    DN~~~~~~=NNNN.ODNNNNDDNNO              ...     .         ...DNNNN=======ND===========DD        ")
-print("       INDN7    DD.     .DD~~~~~=IDND:.:~.....?DNDNN.                        ...... ....$D=======ND===========ND        ")
-print("       NN        ND.    8N=~~~~$ND::.:=~:.~=......=ND~                 .NNNNNNNNNNNNNNN.~N+======NN===========DN        ")
-print("       $DD        DN:   DD~~~~7NO...~==.:~~:.....                      NNNND? ..::..7NZ.:N?======8D~==========ZN        ")
-print("       DN?     ~D: DND.?D~~~~~DD....~:.~=~.......                            ....~=:.:~..ND======~N$==========~DO       ")
-print("       ND    ..DD.  .DNDN=~~~~DI.......:.........                           ....=~..~~~..DN======~DD===========NN       ")
-print("       DDD  :.:DD.  . DDI~~~~~ND................        .DNNNNNNNNNN7      ....=~:.:~~...NN=======ND===========?D~      ")
-print("       8D. ...OD..    DD~~~~~~+ND ............          NN:~::::~~~8N      ........~~...:ND=======DN============NN      ")
-print("       DDI:...ND     .D7~~~~~~~7NN ..........           ID8::::::::8D      .............:DN=======ON============NN      ")
-print("        ~NNND.N=.   .NN~~~~~~~~~NDN8                       ~::::::~N8       .............DN========D=============NI     ")
-print("               DDNNN.ND~~~~~~~~DD =DND                                       ............DN========N+~===========NN     ")
-print("                   ~:N=~~~~~~~~DD   .DDDD                                       ........ NN========DD============8D     ")
-print("                    8N~~~~~~~~~ND    . .7NDDD? .                                      .8DDN========NN=============D:    ")
-print("                    DD~~~~~~~~~DND:         IDNNND$.                           .+DNNNNDNIDN========DD=============DD    ")
-print("                    ND~~~~~~~~ZN 7DD .. .:DDNDDNNDNNNNDDNDND8$?===+$8DDNNNDDDDDN8I~DN====8N========NN=============NN    ")
-print("                    DD~~~~~~~~8N   DD.  .NN~~~~.~~=DNDNO.:7ODDDDNNDD8DDDND=~~~ =~~~ON====8N========DN=============DN    ")
-print("                    ND~~~~~~~~DN    ZDD  DN~~~ ~~~~~=.7DDD+.......8NNN==~~~~~ ~~~~~ONN$==DN========8N=============ON    ")
-print("                    ND~8N~=~~~ZN      DDODN=~.~~~~~=.~~~~INDNNNNDNN~~~~~~~~:~~~~~~~DN~ND=DN========DD=========~ND=8N    ")
-print("                    IN=NDDI~~~~D8       DNN::~~~~~.~~~~~=.~~ND~~ND~~~~~~~~.~~~~~~~~NN  NDNN====ND==ND~D?======DNN=ND    ")
-print("                     DNNI8ND=~~DN:       ZN=~~~~~ ~~~~~.~~~~DD~=DD~~~~~~~ ~~~~~~~=.ND. . ND===DNDD=NDDNN=====8NZDDDN    ")
-print("                      NND  IDNDNNN+       D+~~~:~~~~~~ ~~~~~DDNNN+~~~~~~~~~~~~~~:=?N7   .ND=~ND  DNNN~ID====ND7 NNN     ")
-print("                       ID                 ND~~ ~~~~~:.~~~7DDN7IDNN==~~ ~~~~~~~~ ~~DN   .:N?DDDDD NND  8N~=DDD  ZNN      ")
-print("                                          NN~:~~~~~ =7DDDD+8N  :N8DDZ.~~~~~~~~.~~~DD.   NDD+ . DN=     OND+             ")
-print("                                          DND~~~=8DNDDZ=~~ ND   NN~INND~~~~~.~~~~ND .    .    ..IDD                     ")
-print("                                         DDNNNDNNN+~~~~~~.7N.    ND~~~NDDI~ ~~~~=NNN             .DDI                   ")
-print("                                        DN=~~~~.=~~~~~~ ~~DN     +N+~~~~+DNDD~~~NNNND.            ..ND                  ")
-print("                                         DDI~~ ~~~~~~~ ~~~ND..  ..ND~~~~:~~~DNDNNNN+            ..7O8ND+                ")
-print("                                          .DND=~~~~=::~~=NN.   . . 8D~~.~~~~~~=DN$ODNDNDNNNDNNNNND8+~..                 ")
-print("                                             8DNNI=.~~~~=NDDNNNNDDNDNN.~~~~~IDDNDND7:.                                  ")
-print("                                                ?DNNDD?~DD          ~NN~~=NDD$                                          ")
-print("                                                     :DDD.            NNNN=                                             ")
+import sys
+from collections import deque
+def find_group_block():
+    global graph
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    block_group = {}
+    visited = [[False]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            t = graph[i][j]
+            if t in [0,1,-2]:
+                continue
+            q = deque()
+            q.append([i,j])
+            color_cnt = 1
+            # rainbow_cnt = 0
+            visited2 = [[False]*N for _ in range(N)]
+            visited2[i][j] = True
+            visited[i][j] = True
+            while q:
+                a,b = q.popleft()
+                for d in range(4):
+                    nx = a + dx[d]
+                    ny = b + dy[d]
+                    if nx>=0 and ny>=0 and nx<N and ny<N and not visited2[nx][ny]:
+                        if visited[nx][ny]:
+                            break
+                        if graph[nx][ny] == t or graph[nx][ny] == 0:
+                            visited2[nx][ny] = True
+                            visited[nx][ny] = True
+                            q.append([nx,ny])
+                            if graph[nx][ny] == t:
+                                color_cnt += 1
+                            # if graph[nx][ny] == 0:
+                            #     rainbow_cnt += 1
+
+            temp_block_group =[]
+            if color_cnt > 1:
+                for vi in range(N):
+                    for vj in range(N):
+                        if visited2[vi][vj]:
+                            temp_block_group.append([vi,vj])
+                if color_cnt in block_group:
+                    block_group[color_cnt].append(temp_block_group)
+                else:
+                    block_group[color_cnt] = []
+                    block_group[color_cnt].append(temp_block_group)
+    return block_group
+
+
+
+N,M = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+b = find_group_block()
+b_keys = list(sorted(b.keys()))
+print(b)
+while 1:
+    b = find_group_block()
+    b_keys = list(sorted(b.keys()))
+    target = b[b_keys[-1]]
+    block_idx = 0
+    # 크기가 가장 큰 블록이 1개인 경우
+    if len(target) == 1:
+        for t in target[0]:
+            # 그래프 요소 삭제(삭제 된 애는 -2)
+            graph[t[0]][t[1]] = -2
+    # 크기가 가장 큰 블록이 1개가 아닌경우
+    else:
+        rainbow_cnt = []
+        # 군집 별 무지개 블록수 구하기 
+        for idx,tar in enumerate(target):
+            temp_rainbow_cnt = 0
+            for t in target:
+                if graph[t[0]][t[1]] == 0:
+                    temp_rainbow_cnt += 1
+            rainbow_cnt.append(temp_rainbow_cnt)
+        # 무지개 블록수가 같은게 여러개 인 경우
+        if rainbow_cnt.count(max(rainbow_cnt)) != 1:
+            # 기준 블록 리스트
+            target_block = []
+            for tar in target:
+                for t in tar:
+                    if graph[t[0]][t[1]] != 0:
+                        target_block.append(t)
+                        break
+            result_block = [-1,-1]
+            # 행이 큰거 열이 큰거 찾기
+            for idx,gi in enumerate(target_block):
+                if gi[0] > result_block[0]:
+                    result_block[0], result_block[1] = gi[0], gi[1]
+                    block_idx = idx
+                elif gi[0] == result_block[0]:
+                    if gi[1] > result_block[1]:
+                        result_block[0], result_block[1] = gi[0], gi[1]
+                        block_idx = idx
+        # 무지개 블록수가 같은게 한개인 경우
+        else:
+            block_idx = rainbow_cnt.index(max(rainbow_cnt))
+        
+        # 그래프 에서 삭제
+        for t in target[block_idx]:
+            # 그래프 요소 삭제(삭제 된 애는 -2)
+            graph[t[0]][t[1]] = -2
+
+        # 중력 작용
+        for g_j in range(N):
+            go = 0
+            for g_i in range(N-1,-1,-1):
+                if graph[g_i][g_j] == -2:
+                    go += 1
+                elif graph[g_i][g_j] == -1:
+                    break
+            
+
+
+
+        
+
+
+    # print(target)
+    break
+
+for g in graph:
+    print(g)
+
