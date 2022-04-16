c156f7eaabe846d0cea23d0558c771da7e6de874
park1997 <iambyunghyun97@gmail.com>
2022-04-16 12:53:09 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,164 +1,29 @@
 import sys
-from collections import deque
-def find_group_block():
-    global graph
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    block_group = {}
-    visited = [[False]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            t = graph[i][j]
-            if t in [0,-1,-2]:
-                continue
-            q = deque()
-            q.append([i,j])
-            color_cnt = 1
-            # rainbow_cnt = 0
-            visited2 = [[False]*N for _ in range(N)]
-            visited2[i][j] = True
-            visited[i][j] = True
-            v2_cnt = 1
-            while q:
-                a,b = q.popleft()
-                for d in range(4):
-                    nx = a + dx[d]
-                    ny = b + dy[d]
-                    if nx>=0 and ny>=0 and nx<N and ny<N and not visited2[nx][ny] and not visited[nx][ny]:
-                        if graph[nx][ny] == t or graph[nx][ny] == 0:
-                            v2_cnt += 1
-                            visited2[nx][ny] = True
-                            # rainbow가 아닐때만 방문처리
-                            if graph[nx][ny] != 0:
-                                visited[nx][ny] = True
-                            q.append([nx,ny])
-                            if graph[nx][ny] == t:
-                                color_cnt += 1
-            temp_block_group =[]
-            if color_cnt >= 1 and v2_cnt>=2:
-                for vi in range(N):
-                    for vj in range(N):
-                        if visited2[vi][vj]:
-                            temp_block_group.append([vi,vj])
-                if len(temp_block_group) in block_group:
-                    block_group[len(temp_block_group)].append(temp_block_group)
-                else:
-                    block_group[len(temp_block_group)] = []
-                    block_group[len(temp_block_group)].append(temp_block_group)
-    return block_group
+import heapq
+def Djikstra(start):
+    q = []
+    heapq.heappush(q,[0,start])
+    distance[start] = 0
+    while q:
+        dis,node = heapq.heappop(q)
+        # if distance[node] < dis:
+        #     continue
+        for n,d in graph[node]:
+            new_d = dis + d
+            if distance[n] > new_d:
+                distance[n] = new_d
+                heapq.heappush(q,[new_d,n])
 
-
-
-N,M = map(int,sys.stdin.readline().split())
-graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-b = find_group_block()
-b_keys = list(sorted(b.keys()))
-result = 0
-while 1:
-    b = find_group_block()
-    b_keys = list(sorted(b.keys()))
-    if len(b_keys) ==0:
-        break
-    target = b[b_keys[-1]]
-    block_idx = 0
-    # 크기가 가장 큰 블록이 1개인 경우
-    if len(target) == 1:
-        for t in target[0]:
-            # 그래프 요소 삭제(삭제 된 애는 -2)
-            graph[t[0]][t[1]] = -2
-        result += len(target[0])**2
-    # 크기가 가장 큰 블록이 1개가 아닌경우
+V,E = map(int,sys.stdin.readline().split())
+start = int(sys.stdin.readline())
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    a,b,c = map(int,sys.stdin.readline().split())
+    graph[a].append([b,c])
+distance = [int(1e9)]*(V+1)
+Djikstra(start)
+for i in range(1,len(distance)):
+    if distance[i] != int(1e9):
+        print(distance[i])
     else:
-        rainbow_cnt = []
-        # 군집 별 무지개 블록수 구하기 
-        for idx,tar in enumerate(target):
-            temp_rainbow_cnt = 0
-            for t in tar:
-                if graph[t[0]][t[1]] == 0:
-                    temp_rainbow_cnt += 1
-            rainbow_cnt.append(temp_rainbow_cnt)
-        # 무지개 블록수가 같은게 여러개 인 경우
-        if rainbow_cnt.count(max(rainbow_cnt)) != 1:
-            # 기준 블록 리스트
-            target_block = []
-            for tar in target:
-                for t in tar:
-                    if graph[t[0]][t[1]] != 0:
-                        target_block.append(t)
-                        break
-            result_block = [-1,-1]
-            # 행이 큰거 열이 큰거 찾기
-            for idx,gi in enumerate(target_block):
-                if gi[0] > result_block[0]:
-                    result_block[0], result_block[1] = gi[0], gi[1]
-                    block_idx = idx
-                elif gi[0] == result_block[0]:
-                    if gi[1] > result_block[1]:
-                        result_block[0], result_block[1] = gi[0], gi[1]
-                        block_idx = idx
-        # 무지개 블록수가 같은게 한개인 경우
-        else:
-            block_idx = rainbow_cnt.index(max(rainbow_cnt))
-        
-        # 그래프 에서 삭제
-        for t in target[block_idx]:
-            # 그래프 요소 삭제(삭제 된 애는 -2)
-            graph[t[0]][t[1]] = -2
-        result += len(target[block_idx])**2
-    # 중력 작용
-    for i in range(N-2,-1,-1):
-        for j in range(N):
-            if graph[i][j] > -1:
-                r =i
-                while 1:
-                    if 0<=r+1< N and graph[r+1][j] == -2:
-                        graph[r+1][j] = graph[r][j]
-                        graph[r][j] = -2
-                        r += 1
-                    else:
-                        break
-    temp_graph = [[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            temp_graph[i][j] = graph[j][N-i-1]
-
-    for i in range(N):
-        for j in range(N):
-            graph[i][j] = temp_graph[i][j]
-    # 중력 작용
-    for i in range(N-2,-1,-1):
-        for j in range(N):
-            if graph[i][j] > -1:
-                r =i
-                while 1:
-                    if 0<=r+1< N and graph[r+1][j] == -2:
-                        graph[r+1][j] = graph[r][j]
-                        graph[r][j] = -2
-                        r += 1
-                    else:
-                        break
-
-    # print(target)
-    # break
-
-print(result)
-
-
-'''
-5 4
-2 3 4 -1 -1
-1 -1 4 2 3
-4 3 3 1 1
-4 1 -1 2 2
-1 1 0 -1 0
-'''
-
-
-'''
-5 2
-2 2 2 2 2
-2 2 2 2 2
-2 2 2 2 2
-2 2 2 2 2
-2 2 2 2 2
-'''
+        print("INF")
