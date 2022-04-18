ab4885a55be7fe1d47bd7f611516f1084d7c625c
park1997 <iambyunghyun97@gmail.com>
2022-04-19 05:22:28 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,17 +1,24 @@
 import sys
+from collections import deque
 n = int(sys.stdin.readline())
-dp = [int(1e9)] *(n+1)
-dp[1] = 1
-for i in range(1,n):
-    if i**2<=n:
-        dp[i**2] = 1
+li = [int(sys.stdin.readline()) for _ in range(n)]
+s = deque()
+index = 0
+result = []
+result_num = []
 for i in range(1,n+1):
-    if dp[i] == 1:
-        continue
-    for j in range(1,i+1):
-        if i-j**2>=0:
-            dp[i] = min(dp[i],dp[i-j**2]+dp[j**2])
-        else:
-            break
-print(dp)
-print(dp[-1])
+    s.append(i)
+    result.append("+")
+    if i == li[index]:
+        while s:
+            if s[-1] == li[index]:
+                result.append("-")
+                result_num.append(s.pop())
+                index += 1
+            else:
+                break
+if result_num == li:
+    for r in result:
+        print(r)
+else:
+    print("NO")
