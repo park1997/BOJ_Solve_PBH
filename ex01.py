1d4831f1ad3527e891585fc6edfe16f0c9abe705
park1997 <iambyunghyun97@gmail.com>
2022-04-30 09:14:53 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,16 +1,9 @@
-from itertools import combinations
-ans = 0
-nums = [1, 2, 3, 4]
-sosu = [0] * (max(nums)*3)
-for j in range(2,int((max(nums)*3)**(1/2)),1):
-    mul = 2
-    while mul*j<len(sosu):
-        sosu[mul*j] = 1
-        mul += 1
-    
-numbers = list(combinations(nums,3))
-print(numbers)
-for i in numbers:
-    if sosu[sum(i)] == 0:
-        ans += 1
-print(ans)
+def main():
+    def hello():
+        a[0] += 1
+
+    a = [1]
+
+
+if __name__ == '__main__':
+    main()
