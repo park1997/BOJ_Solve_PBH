88593cf1f8073c2592953ad86e2c42001dd36013
park1997 <iambyunghyun97@gmail.com>
2022-05-02 03:06:41 +0900

    Create 11048.py

---
+++ BOJ_1xxx/11048.py
@@ -0,0 +1,8 @@
+# 이동하기 
+N,M = map(int,input().split())
+data = [list(map(int,input().split())) for i in range(N)]
+dp = [[0]*(M+1)]*(N+1)
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        dp[i][j] = max(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+data[i-1][j-1]
+print(dp[-1][-1])
