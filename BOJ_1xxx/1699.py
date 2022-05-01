7f0e8fa8382e110afad58a78f182692e15f8898e
park1997 <iambyunghyun97@gmail.com>
2022-05-01 15:30:50 +0900

    Create 1699.py

---
+++ BOJ_1xxx/1699.py
@@ -0,0 +1,11 @@
+# 제곱수의 합
+from math import sqrt
+N = int(input())
+dp = [0]*(N+1)
+dp[0] = 0
+dp[1] = 1
+for i in range(2,N+1):
+    dp[i] = i
+    for j in range(1,int(sqrt(i))+1):
+        dp[i] = min(dp[i],dp[i-j*j]+1)
+print(dp[-1])
