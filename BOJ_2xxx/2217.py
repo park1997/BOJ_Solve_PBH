93cc25743675f9986e7a59d293b2543a0c39aac2
park1997 <iambyunghyun97@gmail.com>
2022-05-22 23:15:42 +0900

    Create 2217.py

---
+++ BOJ_2xxx/2217.py
@@ -0,0 +1,11 @@
+# 로프
+N = int(input())
+data = [int(input()) for i in range(N)]
+data.sort()
+result = 0
+result = data[0]*N
+for i in range(1,len(data)):
+    temp = data[i]*(N-i)
+    if result<temp:
+        result = temp
+print(result)
