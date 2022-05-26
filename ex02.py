36bb35ea41c5ad2391bff2bba8bc8f5768272d40
park1997 <iambyunghyun97@gmail.com>
2022-05-26 16:50:36 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,6 +1,32 @@
 import sys
-N,M = map(int,sys.stdin.readline().split())
-l = list(map(int,sys.stdin.readline().split()))
-l2 = [l[i+1]-l[i] for i in range(N-1)]
-l2.sort()
-print(sum(l2[:N-M]))
+import heapq
+def monkey(graph):
+    dx = [-1,-2,-2,-1,1,2,2,1]
+    dy = [-2,-1,1,2,2,1,-1,-2]
+    dx2 = [0,0,1,-1]
+    dy2 = [1,-1,0,0]
+    q = []
+    heapq.heappush(q,[0,0,0,0])
+    visited = [[False]*H for _ in range(W)]
+    visited[0][0] = True
+    while q:
+        print(q)
+        d,k,a,b = heapq.heappop(q)
+        if [a,b] == [W-1,H-1]:
+            return d
+        for i in range(8):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx>=0 and ny>=0 and nx<W and ny<H and not visited[nx][ny] and graph[nx][ny] != 1:
+                heapq.heappush(q,[d+1,nx,ny])
+                visited[nx][ny] = True
+    return -1
+
+K = int(sys.stdin.readline())
+W, H = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(W)]
+
+r = monkey(graph)
+print(r)
+
+
