9eb48b5fa5811328365060e805e18a822dac3b9b
park1997 <iambyunghyun97@gmail.com>
2022-07-04 15:57:07 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,138 +1,34 @@
 import sys
-def destroy(d,s,graph):
-    global center
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    a,b = center
-    for _ in range(s):
-        a += dx[d]
-        b += dy[d]
-        graph[a][b] = 0
-    return graph
+sys.setrecursionlimit(10**9)
 
-def flatten(graph):
-    global center, N
-    fg = [0] * (N ** 2)
-    dx = [0,1,0,-1]
-    dy = [-1,0,1,0]
-    a,b = center
-    go = 1
-    direc = 0
-    index = 1
-    cnt = 0
-    while True:
-        cnt += 1
-        for i in range(go):
-            nx = a + dx[direc]
-            ny = b + dy[direc]
-            a, b = nx, ny
-            if index == N**2 - 1:
-                break    
-            fg[index] = graph[nx][ny]
-            index += 1
-        if index == N**2 - 1:
-            break
-        if cnt % 2 == 0:
-            go += 1
-        direc = (direc + 1) % 4
-    return fg
+n = int(sys.stdin.readline())
+graph = []
+for _ in range(n):
+    arr = list(map(int, sys.stdin.readline().split()))
+    graph.append(arr)
 
-def make_graph(fg, graph):
-    global center, N
-    dx = [0,1,0,-1]
-    dy = [-1,0,1,0]
-    a, b = center
-    go = 1
-    direc = 0
-    index = 1
-    cnt = 0
-    while True:
-        cnt += 1
-        for i in range(go):
-            nx = a + dx[direc]
-            ny = b + dy[direc]
-            a,b = nx, ny
-            graph[nx][ny] = fg[index]
-            if index == N**2 -1:
-                break
-            index += 1
-        if index == N**2 -1:
-            break
-        if cnt % 2 == 0:
-            go += 1
-        direc = (direc + 1) % 4
-    return graph
+def dfs(x,y, h):
+    if x <= -1 or x >= n or y <= -1 or y >= n:
+        return False
+    if graph[x][y] <= h:
+        temp[x][y] = 1
+        return False
+    if temp[x][y] == 0:
+        temp[x][y] = 1
+        dfs(x-1, y, h)
+        dfs(x, y-1, h)
+        dfs(x+1, y, h)
+        dfs(x, y+1, h)
+        return True
+    return False
 
-def change_bead(fg):
-    temp_bead = [0]
-    bead_dic = {1:1,2:1,3:1}
-    target = 0
-    cnt = 1
-    for idx,f in enumerate(fg):
-        if idx == 0:
-            continue
-        if f == target:
-            cnt += 1
-        else:
-            if target != 0:
-                temp_bead.append(cnt)
-                temp_bead.append(target)
-            target = f
-            cnt = 1
-    return temp_bead + [0] * (N**2 - len(temp_bead))
-
-
-N,M = map(int,sys.stdin.readline().split())
-graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-center = [N//2, N//2]
-result = 0
-for _ in range(M):
-    d, s = map(int,sys.stdin.readline().split())
-    graph = destroy(d-1,s,graph)
-    flat_graph = flatten(graph)
-    fg = []
-    for idx,f in enumerate(flat_graph):
-        if idx == 0:
-            fg.append(0)
-        if f != 0:
-            fg.append(f)
-    fg = fg + [0] * (len(flat_graph) - len(fg))
-
-    while True:
-        flag = False
-        target = 0
-        cnt = 0
-        for idx,f in enumerate(fg):
-            if f == target:
-                cnt += 1
-            else:
-                if cnt >= 4 and target != 0:
-                    flag = True
-                    result += target * cnt
-                    for i in range(idx - cnt, idx,1):
-                        fg[i] = 0
-                target = f
-                cnt = 1
-        if not flag:
-            break
-        temp_fg = []
-        for idx,f in enumerate(fg):
-            if idx == 0:
-                temp_fg.append(0)
-            if f != 0:
-                temp_fg.append(f)
-        fg = temp_fg + [0] * (len(fg) - len(temp_fg))
-    
-    fg = change_bead(fg)
-    graph = make_graph(fg, graph)
-
-
-print(result)
-
-
-
-
-
-# new_graph = flatten(graph)
-
-# print(new_graph)
+result = []
+for height in range(max(max(graph))+1):
+    temp = [[0] * n for _ in range(n)]
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] >= height and dfs(i, j, height):
+                count += 1
+    result.append(count)
+print(max(result))
