ef0177e3d02fc8ed222d5f12c01a0788cecc2912
park1997 <iambyunghyun97@gmail.com>
2022-07-14 01:42:33 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -5,7 +5,6 @@ for _ in range(6):
 
 # 6
 result = paper[6]
-# paper[6] = 0
 
 # 5
 for i in range(paper[5]):
@@ -36,15 +35,9 @@ for i in range(paper[4]):
 if paper[3] >= 4:
     result += paper[3] // 4
     paper[3] = paper[3] % 4
-    # while True:
-    #     paper[3] -= 4
-    #     result += 1
-    #     if paper[3] < 4:
-    #         break
 cnt3 = 0
 if paper[3] == 3:
     result += 1
-    # paper[3] -= 3
     if paper[2] > 0:
         paper[2] -= 1
         if paper[1] > 0:
@@ -57,7 +50,6 @@ if paper[3] == 3:
                 paper[1] -= 1
 elif paper[3] == 2:
     result += 1
-    # paper[3] -= 2
     for i in range(3):
         if paper[2] > 0:
             paper[2] -= 1
@@ -67,7 +59,6 @@ elif paper[3] == 2:
             paper[1] -= 1
 elif paper[3] == 1:
     result += 1
-    # paper[3] -= 1
     for i in range(5):
         if paper[2] > 0:
             paper[2] -= 1
@@ -80,31 +71,18 @@ elif paper[3] == 1:
 if paper[2] >= 9:
     result += paper[2] // 9
     paper[2] = paper[2] % 9
-    
-    # while True:
-    #     paper[2] -= 9
-    #     result += 1
-    #     if paper[2] < 9:
-    #         break
 
 if paper[2] > 0:
     result += 1
     for i in range(36 - paper[2]* 4):
         if paper[1] > 0:
             paper[1] -= 1
-    # paper[2] = 0
 
 
 # 1
 if paper[1] >= 36:
     result += paper[1] // 36
     paper[1] = paper[1] % 36
-
-    # while True:
-    #     paper[1] -= 36
-    #     result += 1
-    #     if paper[1] < 36:
-    #         break
 
 if paper[1] > 0:
     result += 1
