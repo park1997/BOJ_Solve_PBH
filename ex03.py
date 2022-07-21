4ce8371fa070f891ea5f81ceca2222db0caa4f44
park1997 <iambyunghyun97@gmail.com>
2022-07-21 21:21:27 +0900

    Update ex03.py

---
+++ ex03.py
@@ -0,0 +1,75 @@
+import sys
+
+from ex01 import sharkMove
+
+def findFishNum(graph,num):
+    for i in range(4):
+        for j in range(4):
+            if graph[i][j][0] == num:
+                return i, j
+
+def findShark(graph):
+    for i in range(4):
+        for j in range(4):
+            if graph[i][j][0] == 0:
+                return i, j
+
+def fishMove():
+    global graph, pass_fish
+    dx = [-1,-1,0,1,1,1,0,-1]
+    dy = [0,-1,-1,-1,0,1,1,1]
+    for move_fish in range(1,17,1):
+        if move_fish == pass_fish:
+            continue
+        
+        x, y = findFishNum(graph, move_fish)
+        direc = graph[x][y][1]
+        cnt = 0
+        while True:
+            nx = x + dx[direc]
+            ny = y + dy[direc]
+            if 0 <= nx < 4 and 0 <= ny < 4 and graph[nx][ny][0] != 0:
+                print(move_fish,nx,ny,i)
+                temp_a, temp_b = graph[nx][ny]
+                graph[nx][ny][0], graph[nx][ny][1] = graph[x][y]
+                graph[x][y][0], graph[x][y][1] = temp_a, temp_b
+                break
+            else:
+                cnt += 1
+                direc = (direc + 1) % 8
+                if cnt == 8:
+                    break
+        
+        for gra in graph:
+            # 박병현 메롱 나도 16인치 사구시땅
+            print(gra)
+        print()
+
+
+def moveShark():
+    global graph
+
+
+
+
+    return 
+
+
+graph = []
+for i in range(4):
+    a1,b1, a2,b2, a3,b3, a4,b4 = map(int,sys.stdin.readline().split())
+    graph.append([[a1, b1 - 1],[a2, b2 - 1],[a3, b3 - 1],[a4, b4 - 1]])
+
+
+pass_fish = graph[0][0][0]
+graph[0][0] = [0, graph[0][0][1]]
+
+
+
+
+for gra in graph:
+    print(gra)
+
+while 1:
+    fishMove()
+    sharkMove()
