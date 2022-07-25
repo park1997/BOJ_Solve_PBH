32e43cef4b0d0d4f6c94f21b37b603528ade6c45
park1997 <iambyunghyun97@gmail.com>
2022-07-25 14:08:56 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,27 +1,56 @@
 import sys
-N, M, k = map(int, sys.stdin.readline().split())
-graph = [[0] * N for _ in range(N)]
-shark_posi = {}
+from collections import deque
 
-for i in range(N):
-    gra = list(map(int,sys.stdin.readline().split()))
-    for j in range(N):
-        if gra[j] != 0:
-            shark_posi[gra[j]] = [i,j]
+def bfs(n):
+    global k
+    q = deque()
+    count = 0
+    q.append([count, n])
+    visited[n] = True
 
+    if n == k:
+        return count
 
+    while q:
 
-# 위 아래 왼 오 = (1,2,3,4)
-shark_first_direc = {0 : 0}
-sfd = list(map,int, sys.stdin.readline().split())
-for idx, s in enumerate(sfd):
-    shark_first_direc[idx] = s
+        count, subin_location = q.popleft()
+        print(count, subin_location)
+        local_count = 0
 
-
-shark_direc = {}
-
-for i in range(1, M + 1, 1):
-    d = list(map(int,sys.stdin.readline().split()))
-    for j, ele in enumerate(d):
+        teleportation = subin_location * 2
+        minus_1 = subin_location - 1
+        plus_1 = subin_location + 1
         
+        
+        if teleportation == k:
+            return count
 
+        if minus_1 == k:
+            local_count = count + 1
+            return local_count
+
+        if plus_1 == k:
+            local_count = count + 1
+            return local_count
+
+        if 0 <= teleportation <= 100000 and visited[teleportation] == False:
+            local_count = count
+            q.append([local_count, teleportation])
+            visited[teleportation] = True
+
+        if 0 <= minus_1 <= 100000 and visited[minus_1] == False:
+            local_count = count + 1
+            q.append([local_count, minus_1])
+            visited[minus_1] = True
+
+        if 0 <= plus_1 <= 100000 and visited[plus_1] == False:
+            local_count = count + 1
+            q.append([local_count, plus_1])
+            visited[plus_1] = True
+
+n, k = map(int, sys.stdin.readline().split())
+
+visited = [False] * 100001
+
+result = bfs(n)
+print(result)
