cf67c20e255e80af7944eeab09faa62a06b1bdb5
park1997 <iambyunghyun97@gmail.com>
2022-07-27 17:10:09 +0900

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,56 +1,17 @@
-import sys
-from collections import deque
+import heapq
 
-def bfs(n):
-    global k
-    q = deque()
-    count = 0
-    q.append([count, n])
-    visited[n] = True
+q = []
 
-    if n == k:
-        return count
+heapq.heappush(q, [2,2,3])
+heapq.heappush(q, [1,2,3])
+heapq.heappush(q, [3,2,3])
+heapq.heappush(q, [1,4,3])
+heapq.heappush(q, [5,2,3])
+heapq.heappush(q, [1,6,3])
 
-    while q:
-
-        count, subin_location = q.popleft()
-        print(count, subin_location)
-        local_count = 0
-
-        teleportation = subin_location * 2
-        minus_1 = subin_location - 1
-        plus_1 = subin_location + 1
+while q:
+    cnt, x, y = heapq.heappop(q)
+    if 벽
+        heapq.heappush(q, [cnt +1 , nx ,ny ])
         
-        
-        if teleportation == k:
-            return count
-
-        if minus_1 == k:
-            local_count = count + 1
-            return local_count
-
-        if plus_1 == k:
-            local_count = count + 1
-            return local_count
-
-        if 0 <= teleportation <= 100000 and visited[teleportation] == False:
-            local_count = count
-            q.append([local_count, teleportation])
-            visited[teleportation] = True
-
-        if 0 <= minus_1 <= 100000 and visited[minus_1] == False:
-            local_count = count + 1
-            q.append([local_count, minus_1])
-            visited[minus_1] = True
-
-        if 0 <= plus_1 <= 100000 and visited[plus_1] == False:
-            local_count = count + 1
-            q.append([local_count, plus_1])
-            visited[plus_1] = True
-
-n, k = map(int, sys.stdin.readline().split())
-
-visited = [False] * 100001
-
-result = bfs(n)
-print(result)
+    print(cnt,x,y)
