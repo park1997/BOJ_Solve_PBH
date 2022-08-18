fad2dfc725f617962ade3298b22ae13c3f4a8606
park1997 <iambyunghyun97@gmail.com>
2022-08-19 05:00:44 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,80 +1,43 @@
-import sys
 from collections import deque
-from itertools import combinations
-def bfs(lst):
-    global visited, graph, time
-    q = deque()
-    for i in lst:
-        q.append((i[0], i[1]))
-        visited[i[0]][i[1]] = 1
+import sys
 
-    while q:
-        x,y = q.popleft()
+def bfs() :
+    result = 10000000
+    que = deque()
+    que.append([0,0,0])
+
+    visited[0][0]= True
+
+    while que:
+        x,y,count= que.popleft()
+        if x== N-1 and y == M-1 and count<=T :
+            return min(count,result)
+        count +=1
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx>=n or nx<0 or ny>=n or ny<0:
-                continue
-            if graph[nx][ny] == '-': # 벽 만나면 continue
-                continue
-            if (nx,ny) in lst: # 활성 바이러스 만나면 continue
-                continue
-            if not visited[nx][ny]: # 비활성 또는 빈칸이면서 방문 안한 곳이라면
-                time[nx][ny] = time[x][y] + 1
-                visited[nx][ny] = 1
-                q.append((nx,ny))
-            else: # 비활성 또는 빈칸이면서 이미 방문한 곳이라면
-                if time[nx][ny] > time[x][y] + 1: # 다른 곳에서 전염된게 더 짧은경우
-                    time[nx][ny] = time[x][y] + 1
-                    q.append((nx,ny))
-    flag = True
-    max_time = 0
-    for i in range(n):
-        for j in range(n):
-            if (i,j) in virus_loc:
-                time[i][j] = 0
-            if graph[i][j] == 100 and time[i][j] == 0:
-                flag = False # 빈칸인데 바이러스 퍼지지 못한경우
-                return 0, flag
-            max_time = max(max_time, time[i][j])
-    return max_time, flag
+            if 0<= nx < N and  0 <= ny< M and not visited[nx][ny] :
+                if graph[nx][ny] == 0 :
+                    que.append([nx,ny,count])
+                    visited[nx][ny] = True
+                    
+                elif graph[nx][ny]==2  :
+                    que.append([nx,ny,count])
+                    visited[nx][ny] = True
+                    result = min(result, (count + abs(N-nx-1) + abs(M-ny-1)))
 
+    if result != 10000000:
+        return result
+
+    return "Fail"    
+                
 input = sys.stdin.readline
-n,m = map(int, input().split())
-graph = []
-virus_loc = []
-for i in range(n):
-    lst = list(map(int, input().split()))
-    for j in range(n):
-        if lst[j] == 2:
-            virus_loc.append((i,j))
-            lst[j] = '*' # *은 비활성 바이러스
-        elif lst[j] == 1:
-            lst[j] = '-' # -은 벽
-        else:
-            lst[j] = 100 # 100은 빈칸
-    graph.append(lst)
+N,M,T = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(N)]
+visited =[[False] * M for _ in range(N)]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+print(bfs())
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-virus_list = combinations(virus_loc, m)
-
-answer = int(1e9)
-for lst in virus_list:
-    visited = [[0] * n for _ in range(n)]
-    time = [[0] * n for _ in range(n)]
-    xylist = []
-    for k in range(m):
-        x_, y_ = lst[k][0], lst[k][1]
-        xylist.append((x_,y_))
-
-    result, flag = bfs(xylist)
-    if flag:
-        answer = min(answer, result)
-
-if answer != int(1e9):
-    print(answer)
-else:
-    print(-1)
