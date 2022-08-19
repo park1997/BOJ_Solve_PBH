77aa6bca15c93e7847ba1e64c5de49065cfa7ab1
park1997 <iambyunghyun97@gmail.com>
2022-08-19 11:41:45 +0900

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,104 +1,197 @@
+import sys
 from collections import deque
 
-dy = (0, 1, 0, -1)
-dx = (1, 0, -1, 0)
 
-def rotate_and_melting(board, len_board, L):
-    """
-    Level에 맞게 회전 후 얼음을 녹임
-    :param board: 보드
-    :param len_board: 보드 길이
-    :param L: level
-    :return:
-    """
-    new_board = [[0] * len_board for _ in range(len_board)] # 회전한 Board 저장 용
+def newGame2():
+    global position, mal, N, M
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+    for num in range(1, K+1, 1):
+        x, y, direction = mal[num]
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        # 체스판을 벗어나지 않는 경우
+        if nx >= 0 and ny >= 0 and nx < N and ny< N:
+            # 흰색인 경우
+            if color_graph[nx][ny] == 0:
+                temp_queue = deque()
+                for i in range(len(position[x][y])):
+                    temp_num, temp_direction = position[x][y].popleft()
+                    temp_queue.append([temp_num, temp_direction])
+                    mal[temp_num] = [nx, ny, temp_direction]
+                    if temp_num == num:
+                        break
+                for elem_num, elem_direc in position[nx][ny]:
+                    temp_queue.append([elem_num, elem_direc])
+                position[nx][ny] = temp_queue
+                if len(position[nx][ny]) >= 4:
+                    return True
+                
+            # 빨간색인 경우
+            elif color_graph[nx][ny] == 1:
+                for i in range(len(position[x][y])):
+                    temp_num, temp_direction = position[x][y].popleft()
+                    position[nx][ny].appendleft([temp_num, temp_direction])
+                    mal[temp_num] = [nx, ny, temp_direction]
+                    if len(position[nx][ny]) >= 4:
+                        return True
+                    if temp_num == num:
+                        break
+            # 파란색인 경우
+            elif color_graph[nx][ny] == 2:
+                nx = x - dx[direction]
+                ny = y - dy[direction]
+                # 방향 반대로 바꾸기
+                if direction == 0:
+                    direction = 1
+                elif direction == 1:
+                    direction = 0
+                elif direction == 2:
+                    direction = 3
+                elif direction == 3:
+                    direction = 2
+                if nx >= 0 and ny >= 0 and nx < N and ny < N:
+                    # 방향 바꿨는데 흰색인 경우
+                    if color_graph[nx][ny] == 0:
+                        temp_queue = deque()
+                        for i in range(len(position[x][y])):
+                            temp_num, temp_direction = position[x][y].popleft()
+                            temp_queue.append([temp_num, temp_direction])
+                            mal[temp_num] = [nx, ny, temp_direction]
+                            # print("hi",direction,[nx,ny],num)
+                            if temp_num == num:
+                                temp_queue[-1][1] = direction
+                                mal[num] = [nx, ny, direction]
+                                break
+                        for elem_num, elem_direc in position[nx][ny]:
+                            temp_queue.append([elem_num, elem_direc])
+                        position[nx][ny] = temp_queue
+                        if len(position[nx][ny]) >= 4:
+                            return True
+                    # 방향 바꿨는데 빨간색인 경우
+                    elif color_graph[nx][ny] == 1:
+                        for i in range(len(position[x][y])):
+                            temp_num, temp_direction = position[x][y].popleft()
+                            position[nx][ny].appendleft([temp_num, temp_direction])
+                            mal[temp_num] = [nx, ny, temp_direction]
+                            if len(position[nx][ny]) >= 4:
+                                return True
+                            if temp_num == num:
+                                position[nx][ny][0][1] = direction
+                                mal[num] = [nx, ny, direction]
+                                break
+                    # 방향 바꿨는데 파란색인 경우 => 이동 X
+                    elif color_graph[nx][ny] == 2:
+                        mal[num][2] = direction
+                        for j in range(len(position[x][y])):
+                            if position[x][y][j][0] == num:
+                                position[x][y][j][1] = direction
+                                break
+                # 방향 바꿨는데 외부로 벗어나는 경우 => 이동 X
+                else:
+                    mal[num][2] = direction
+                    for j in range(len(position[x][y])):
+                        if position[x][y][j][0] == num:
+                            position[x][y][j][1] = direction
+                            break
+                
+        # 체스판을 벗어나는 경우 => 파란색 => 방향 전환
+        else:
+            nx = x - dx[direction]
+            ny = y - dy[direction]
+            # 방향 반대로 바꾸기
+            if direction == 0:
+                direction = 1
+            elif direction == 1:
+                direction = 0
+            elif direction == 2:
+                direction = 3
+            elif direction == 3:
+                direction = 2
+            # 벽만나서 방향 바꿨는데 흰색인 경우
+            if color_graph[nx][ny] == 0:
+                temp_queue = deque()
+                for i in range(len(position[x][y])):
+                    temp_num, temp_direction = position[x][y].popleft()
+                    temp_queue.append([temp_num, temp_direction])
+                    mal[temp_num] = [nx, ny, temp_direction]
+                    if temp_num == num:
+                        temp_queue[-1][1] = direction
+                        mal[num] = [nx, ny, direction]
+                        break
+                for elem_num, elem_direc in position[nx][ny]:
+                    temp_queue.append([elem_num, elem_direc])
+                position[nx][ny] = temp_queue
+                if len(position[nx][ny]) >= 4:
+                    return True
+            # 벽만나서 방향 바꿨는데 빨간색인 경우
+            elif color_graph[nx][ny] == 1:
+                for i in range(len(position[x][y])):
+                    temp_num, temp_direction = position[x][y].popleft()
+                    position[nx][ny].appendleft([temp_num, temp_direction])
+                    mal[temp_num] = [nx, ny, temp_direction]
+                    if len(position[nx][ny]) >= 4:
+                        return True
+                    if temp_num == num:
+                        position[nx][ny][0][1] = direction
+                        mal[num] = [nx, ny, direction]
+                        break
+            # 벽만나서 방향 바꿨는데 파란색인 경우
+            elif color_graph[nx][ny] == 2:
+                mal[num][2] = direction
+                for j in range(len(position[x][y])):
+                    if position[x][y][j][0] == num:
+                        position[x][y][j][1] = direction
+                        break
+                
 
-    # rotate
-    r_size = 2 ** L # 격자 사이즈
-    for y in range(0, len_board, r_size): # 격자 시작 좌표 y축
-        for x in range(0, len_board, r_size): # 격자 시작 좌표 x축
-            for i in range(r_size): # 열 인덱스
-                for j in range(r_size): # 행 인덱스
-                    new_board[y + j][x + r_size - i - 1] = board[y + i][x + j]
+        # print(num)
+        # print(mal)
+        # for p in position:
+        #     # print(p)
+        #     for t in p:
+        #         print(len(t), end = " ")
+        #     print()
+        # for p in position:
+        #     print(p)
+        # print()
 
-    board = new_board
-    melting_list = [] # 녹을 얼음 좌표
-    for y in range(len_board):
-        for x in range(len_board):
-            ice_count = 0
-            for d in range(len(dy)):
-                ny = y + dy[d]
-                nx = x + dx[d]
-
-                if nx < 0 or ny < 0 or nx >= len_board or ny >= len_board:
-                    continue
-                elif board[ny][nx] > 0:
-                    ice_count += 1
-
-            if ice_count < 3 and board[y][x] != 0:
-                melting_list.append((y, x))
-    print(melting_list)
-    for b in board:
-        print(b)
-    print()
-
-    # 저장된 얼음들을 녹임
-    for y, x in melting_list:
-        board[y][x] -= 1
-    for b in board:
-        print(b)
-    print()
-    return board
-
-def check_ice_bfs(board, len_board):
-    """
-    얼음 상태 확인
-    :param board: 보드
-    :param len_board: 보드 가로 길이
-    :return:
-    """
-    used = [[False] * len_board for _ in range(len_board)]
-    ice_sum = 0
-    max_area_count = 0
-    for y in range(len_board):
-        for x in range(len_board):
-            area_count = 0
-            if used[y][x] or board[y][x] == 0:
-                continue
-            # BFS를 이용하여 얼음 덩어리 조사
-            q = deque()
-            q.append((y, x))
-            used[y][x] = True
-
-            while q:
-                sy, sx = q.popleft()
-                ice_sum += board[sy][sx] # 얼음 합 추가
-                area_count += 1  # 얼음 카운트 추가
-
-                for d in range(4):
-                    ny = sy + dy[d]
-                    nx = sx + dx[d]
-                    if nx < 0 or ny < 0 or nx >= len_board or ny >= len_board or used[ny][nx]:
-                        continue
-                    if board[ny][nx] != 0:
-                        used[ny][nx] = True
-                        q.append((ny, nx))
-
-            max_area_count = max(max_area_count, area_count) # 최대 얼음 덩어리 크기 파악
-
-    print(ice_sum)
-    print(max_area_count)
+    return False
 
 
-def solve():
-    N, Q = map(int, input().split(' '))
-    len_board = 2 ** N
-    board = [list(map(int, input().split(' '))) for _ in range(len_board)]
-    L_list = list(map(int, input().split(' ')))
 
-    for L in L_list:
-        board = rotate_and_melting(board, len_board, L)
+N, K = map(int,input().split())
 
-    check_ice_bfs(board, len_board)
+color_graph = [list(map(int,input().split())) for _ in range(N)]
+position = [[deque() for _ in range(N)] for _ in range(N)]
+mal = {}
+for num in range(1, K + 1, 1):
+    x, y, direc = map(int,input().split())
+    mal[num] = [x - 1, y - 1, direc - 1]
+    position[x-1][y-1].append([num, direc-1])
+# for p in position:
+#     print(p)
+# print()
+count = 0
+while 1:
+    count += 1
+    result_flag = newGame2()
+    if result_flag:
+        # for p in position:
+        #     # print(p)
+        #     for t in p:
+        #         print(len(t), end = " ")
+        #     print()
+        print(count)
+        break
+    if count == 1000:
+        # for p in position:
+        #     print(p)
+        print(-1)
+        break
+    # break
+    # for p in position:
+    #     print(p)
+    # print("*"*10)
+    # print(mal)
 
-solve()
