ce8fea5f8f46e4c6b821171a3d1338695478103e
park1997 <iambyunghyun97@gmail.com>
2022-10-11 17:20:32 +0900

    Update ex01.py

--- Algorithm/SAMSUNG/exam/ex01.py
+++ Algorithm/SAMSUNG/exam/ex01.py
@@ -16,17 +16,7 @@ def bfs(x,y):
                 q.append([nx,ny])
                 last = [nx, ny]
     return last
-def findMaxrix(xs, ys, xd, yd):
-    global graph
-    matrix = []
-    for i in range(xs, xs + xd, 1):
-        temp = []
-        for j in range(ys, ys + yd, 1):
-            temp.append(graph[i][j])
-        matrix.append(temp)
-    return matrix
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N = int(input())
     graph = [list(map(int, input().split())) for _ in range(N)]
@@ -36,13 +26,12 @@ for test_case in range(1, T + 1):
         for j in range(N):
             if not visited[i][j] and graph[i][j] != 0:
                 end_x, end_y = bfs(i,j)
-                print(i,j, end_x, end_y)
                 row = abs(end_x - i)
                 col = abs(end_y - j)
                 case.append([i, j, row + 1, col + 1])
-    print(case)
+    
     c = list(permutations(case, len(case)))
-    print(c)
+    result = int(1e9)
     for c in list(permutations(case, len(case))):
         flag = True
         for d in range(len(case) - 1):
@@ -50,13 +39,19 @@ for test_case in range(1, T + 1):
                 flag = False
                 break
         if flag:
-            print(c)
+            temp = 0
+            new_xd = c[0][2]
             for d in range(len(case) - 1):
                 xs1, ys1, xd1, yd1 = c[d]
                 xs2, ys2, xd2, yd2 = c[d + 1]
-                mat1 = findMaxrix(xs1, ys1, xd1, yd1)
-                mat2 = findMaxrix(xs2, ys2, xd2, yd2)
-            
-
-
-
+                temp += new_xd * xd2 * yd2
+                new_xd = xd1
+            new_xd = c[-1][3]
+            temp2 = 0
+            for d in range(len(case) - 1, 0, -1):
+                xs1, ys1, xd1, yd1 = c[d]
+                xs2, ys2, xd2, yd2 = c[d - 1]
+                temp2 += xd2 * xd1 * new_xd
+                new_xd = yd1
+            result = min(result, temp, temp2)
+    print("#{} {}".format(test_case, result))
