300f1addf02eeb28cd8608a388ab3895ac49d719
park1997 <iambyunghyun97@gmail.com>
2022-10-12 21:59:53 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,107 +1,41 @@
 import sys
-from heapq import heappush,heappop
+import heapq
+def prim(start):
+    global n
+    q = []
+    heapq.heappush(q, [0, start])
+    result = 0
+    visited = [False] * (n)
+    while q:
+        cur_dis, cur_node = heapq.heappop(q)
+        if visited[cur_node]:
+            continue
+        result += cur_dis
+        visited[cur_node] = True
+        for d, n in graph[cur_node]:
+            if not visited[n]:
+                heapq.heappush(q, [d, n])
+    return result
 
-def direction(x,y,n):
+n = int(sys.stdin.readline())
+point = []
+graph = []
+for _ in range(n):
+    a, b = map(float, sys.stdin.readline().split())
+    point.append([a,b])
+    graph.append([])
 
-    if n == 0:
-        return x+1,y
-    elif n == 1:
-        return x-1,y
-    elif n == 2:
-        return x,y+1
-    else:
-        return x,y-1
+for i in range(n - 1):
+    for j in range(i, n, 1):
+        if i == j:
+            continue
+        start = point[i]
+        end = point[j]
+        dis = ((abs(start[0] - end[0]) ** 2) + (abs(start[1] - end[1])) ** 2) ** (1/2)
+        graph[i].append([dis, j])
+        graph[j].append([dis, i])
 
-def reverse(n):
-    if n == 0:
-        return 1
-
-    elif n == 1:
-        return 0
-
-    elif n == 2:
-        return 3
-
-    else:
-        return 2
-
-
-input = sys.stdin.readline
-
-N = int(input())
-arr = [list(input().strip()) for _ in range(N)]
-door = []
-h = []
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-visited = [[[0]*N for _ in range(N)] for _ in range(4)]
-flag = False
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == '#':
-            if not flag:
-                start_x,start_y = i,j
-                flag = True
-            else:
-                end_x,end_y = i,j
-
-
-
-
-cnt = 0
-for dir in range(4):
-    a,b = start_x+dx[dir],start_y+dy[dir]
-
-    if 0 <= a < N and 0 <= b < N and not visited[dir][a][b] and arr[a][b] != '*':
-        
-        visited[dir][a][b] = 1
-        heappush(door,[0,a,b,dir])
-
-        if arr[a][b] == '.':
-            heappush(door,[cnt,a,b,dir])
-            visited[dir][a][b] = 1
-        else:
-            for i in range(4):
-                if reverse(dir) == i:
-                    continue 
-                if i == dir:
-                    heappush(door,[cnt,a,b,dir])
-                else:
-                    heappush(door,[cnt+1,a,b,i])
-            visited[dir][a][b] = 1    
-
-
-# ===========================================================================
-
-answer = float('inf')
-while door:
-
-    cnt,x,y,dir = heappop(door)
-    
-    if [x,y]==[end_x,end_y]:
-        answer = cnt
-        break
-        
-    a,b = direction(x,y,dir)
-    
-    if 0 <= a < N and 0 <= b < N and not visited[dir][a][b] and arr[a][b] != '*':
-        
-        if arr[a][b] == '.':
-            heappush(door,[cnt,a,b,dir])
-            visited[dir][a][b] = 1
-        else:
-            for i in range(4):
-                if reverse(dir) == i:
-                    continue 
-                if i == dir:
-                    heappush(door,[cnt,a,b,dir])
-                else:
-                    heappush(door,[cnt+1,a,b,i])
-            visited[dir][a][b] = 1
-
-print(answer)
-
-
+result = prim(0)
+print("{:.2f}".format(result))
 
 
