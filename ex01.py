e0f1565fdd333f811dbb25197950346933ca99c2
park1997 <iambyunghyun97@gmail.com>
2023-02-12 00:21:55 +0900

    BOJ_raiser_comunication

--- ex01.py
+++ ex01.py
@@ -1,49 +1,39 @@
 import sys
 import heapq
-from collections import deque
-
-def wall3(start):
-    global N, M, K
+def Djikstra():
+    q = []
+    count = 0
+    heapq.heappush(q,[count, start[0], start[1]])
+    visited[start[0]][start[1]] = 0
     dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    q = deque()
-    q.append([1, 0, 0, 0, "M"]) # [이동거리, x, y, 벽 부순횟수, 낮 or 밤]
-    visited[0][0][0] = 1
+    dy = [1, -1, 0 , 0]
     while q:
-        dis, x, y, wall_count, time = q.popleft()
-        print([dis, x, y, wall_count, time])
-        if [x, y] == [N - 1, M - 1]:
-            return dis
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0 <= nx < N and 0<= ny < M:
-                if graph[nx][ny] ==  1 and visited[wall_count + 1][nx][ny] == 0:
-                    if time == "M" and wall_count < K:
-                        visited[wall_count + 1][nx][ny] = dis + 1
-                        q.append([dis + 1, nx, ny, wall_count + 1, "N"])
-                    elif time == "N":
-                        q.append([dis + 1, x, y, wall_count, "M"])
-                        visited[wall_count][x][y] = dis + 1
-                elif graph[nx][ny] == 0:
-                    if time == "M" and visited[wall_count][nx][ny] == 0:
-                        q.append([dis + 1, nx, ny, wall_count, "N"])
-                        visited[wall_count][nx][ny] = dis + 1
-                    elif time == "N" and visited[wall_count][nx][ny] == 0:
-                        q.append([dis + 1, nx, ny, wall_count, "M"])
-                        visited[wall_count][nx][ny] = dis + 1
-    return -1
-
-N, M, K = map(int, sys.stdin.readline().split())
-visited = [[[0] * M for _ in range(N)] for __ in range(K + 1)]
-graph = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-result = wall3([0,0])
-
-for i in range(K):
-    for v in visited[i]:
-        print(v)
-    print()
+        for _ in range(len(q)):
+            cnt, a, b = heapq.heappop(q)
+            if [a,b] == end:
+                return count - 1
+            for i in range(4):
+                nx , ny = a, b
+                while 1:
+                    nx += dx[i]
+                    ny += dy[i]
+                    if not(0 <= nx < n and 0 <= ny < m):
+                        break
+                    if graph[nx][ny] == '*':
+                        break
+                    if visited[nx][ny] > cnt:
+                        visited[nx][ny] = cnt
+                        heapq.heappush(q, [count, nx, ny])
+        count += 1
+m, n = map(int, sys.stdin.readline().split())
+graph = []
+visited = [[int(1e9)] * m for _ in range(n)]
+point = []
+for i in range(n):
+    graph.append(list(sys.stdin.readline().rstrip()))
+    for j in range(m):
+        if graph[i][j] == 'C':
+            point.append([i,j])
+start, end = point
+result = Djikstra()
 print(result)
-
-
-
